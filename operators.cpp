efb9f99cdb5fa60f0f10c1f3ba1208daaf910f03
Dhoni Patel <iamdhonni@gmail.com>
2026-01-02 07:56:19 +0530

    change

--- operators.cpp
+++ operators.cpp
@@ -1,14 +1,38 @@
+// #include <iostream>
+// // MACRO aek symbolic constant hota hai
+// // #define x 25
+// using namespace std;
+
+// int main () {
+
+// // const int num = 25; // must be intialized
+// // int num2 = 25;
+// // num2 = 35;
+// // num = 35; // constant never change
+
+// // const int y = 35;
+
+
+
+    
+//     return 0;
+
+// }
+
+
+// TYPECASTING IN C++
+
 #include <iostream>
 using namespace std;
 
-int main () {
+int main () { 
 
-// const int num = 25;
-// int num2 = 25;
-// num2 = 35;
-// num = 35; // constant never change
+// cout<< (10/3) << endl; //3 
+// cout<< (10/3.0) << endl; //3.3333
+cout << ('A' + 1) << endl; //65
+cout << ('a' + 1) << endl; //65
 
-    
-    return 0;
+cout << ('A' + 0) << endl; //65
+cout << ('a' + 0) << endl; //97
 
 }
