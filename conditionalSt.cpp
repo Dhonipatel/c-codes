703200a018185efed9cae2f8a4fc83abde8adfaa
Dhoni Patel <iamdhonni@gmail.com>
2026-01-05 23:05:49 +0530

    change

--- conditionalSt.cpp
+++ conditionalSt.cpp
@@ -3,56 +3,91 @@ using namespace std;
 
 int main () {
 
-    int age = 18;
-    cout << "Enter your age;";
+    // /////////////// IF ELSE STATEMENT //////////////////////
 
-    cin >> age;
-    if (age >= 18) {
-        cout << "you can vote " << endl;
-    }else{
-        cout << "you can not vote " <<endl;
-    }
+    // int age = 18;
+    // cout << "Enter your age;";
 
-
-    int marks = 33;
-    cout<<"Enter your marks :";
-    cin>> marks;
-
-    if(marks >= 33) {
-        cout<<"you are pass"<<endl;
-    } else{
-        cout<<"you are fail"<<endl;
-    }
+    // cin >> age;
+    // if (age >= 18) {
+    //     cout << "you can vote " << endl;
+    // }else{
+    //     cout << "you can not vote " <<endl;
+    // }
 
 
-    int a , b ;
+    // int marks = 33;
+    // cout<<"Enter your marks :";
+    // cin>> marks;
+
+    // if(marks >= 33) {
+    //     cout<<"you are pass"<<endl;
+    // } else{
+    //     cout<<"you are fail"<<endl;
+    // }
+
+
+    // int a , b ;
    
     
-    cout<<"enter your number a: " ;
-     cin>> a;
+    // cout<<"enter your number a: " ;
+    //  cin>> a;
 
-    cout<< "enter your number b: ";
-    cin>> b;
+    // cout<< "enter your number b: ";
+    // cin>> b;
 
-    if (a >= b) {
-        cout<< "a is largest";
+    // if (a >= b) {
+    //     cout<< "a is largest";
 
-    }else{
-        cout<<"b is largest"<< endl;
-    }
+    // }else{
+    //     cout<<"b is largest"<< endl;
+    // }
 
 
 
-    int num ;
-    cout<< "enter your number: ";
-    cin>> num;
+    // int num ;
+    // cout<< "enter your number: ";
+    // cin>> num;
 
-    if (num % 2 == 0) {
-        cout<< "number is even " <<endl;
+    // if (num % 2 == 0) {
+    //     cout<< "number is even " <<endl;
 
-    }else {
-        cout<< " number is odd"<< endl;
-    }
+    // }else {
+    //     cout<< " number is odd"<< endl;
+    // };
+
+
+
+    // //////////////// ELSE IF STATEMENT ///////////////////
+
+
+    // int age ;
+    // cout << "enter your age:";
+    // cin >> age;
+
+    // if(age < 18 ) {
+    //     cout<< "your are kids" <<endl;
+
+    // } else if (age >= 18 && age < 60) {
+    //     cout<<"you are adult"<<endl;
+    // } else{
+    //     cout<<"you are senior citizen "<< endl;
+    // };
+
+
+    int marks ;
+    cout<<"enter your marks : ";
+ 
+    cin>> marks;
+
+    if(marks > 90 ) {
+        cout << "grade A++ "<< endl;
+    }else if (marks >= 80) {
+        cout << "grade A " <<endl;
+
+    } else{
+        cout << "grade B" << endl;
+    };
 
 
 
