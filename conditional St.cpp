115d5a44e5d1801eafc5ea29ec98266e5b80be3a
Dhoni Patel <iamdhonni@gmail.com>
2026-01-05 20:50:00 +0530

    change

---
+++ conditional St.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+
+    int age = 18;
+    if (age >= 18) {
+        cout << "you can vote " << endl;
+    }else{
+        cout << "you can not vote " <<endl;
+    }
+
+  
+};
