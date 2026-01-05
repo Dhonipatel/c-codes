9e33f8a61643d8e2a93a0fa59a6e43a139cca773
Dhoni Patel <iamdhonni@gmail.com>
2026-01-05 21:24:52 +0530

    change

--- conditionalSt.cpp
+++ conditionalSt.cpp
@@ -14,6 +14,48 @@ int main () {
     }
 
 
+    int marks = 33;
+    cout<<"Enter your marks :";
+    cin>> marks;
+
+    if(marks >= 33) {
+        cout<<"you are pass"<<endl;
+    } else{
+        cout<<"you are fail"<<endl;
+    }
+
+
+    int a , b ;
+   
+    
+    cout<<"enter your number a: " ;
+     cin>> a;
+
+    cout<< "enter your number b: ";
+    cin>> b;
+
+    if (a >= b) {
+        cout<< "a is largest";
+
+    }else{
+        cout<<"b is largest"<< endl;
+    }
+
+
+
+    int num ;
+    cout<< "enter your number: ";
+    cin>> num;
+
+    if (num % 2 == 0) {
+        cout<< "number is even " <<endl;
+
+    }else {
+        cout<< " number is odd"<< endl;
+    }
+
+
+
     return 0;
 
   
