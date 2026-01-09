b8879595ab1a46e3449276a3e93d61a84c225e9c
Dhoni Patel <iamdhonni@gmail.com>
2026-01-09 20:27:41 +0530

    change

--- loops.cpp
+++ loops.cpp
@@ -1,7 +1,15 @@
 #include <stdio.h>
 int main() {
-    for(int i = 1; i <= 5; i++) {
-        printf("%d\n", i);
-    }
-    return 0;
+    // for(int i = 1; i <= 5; i++) {
+    //     printf("%d\n", i);
+    // }
+    // return 0;
+
+    int i = 1;
+do {
+    printf("%d\n", i);
+    i++;
+} while(i <= 5);
+
+
 }
