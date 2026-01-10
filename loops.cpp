b408876f547689ec53c0d870daa108cf9767f762
Dhoni Patel <iamdhonni@gmail.com>
2026-01-10 08:28:58 +0530

    change

--- loops.cpp
+++ loops.cpp
@@ -5,13 +5,29 @@ int main() {
 
     // FOR LOOPS
 
-    int num;
+    // int num;
 
-     for(num = 1; num >=0; num++) {
-        cout<< num<< " ";
-     }
+    //  for(num = 1; num >=0; num++) {
+    //     cout<< num<< " ";
+    //  }
 
-     cout<< endl;
-     cout << "last value of num =" << num << endl;
+    //  cout<< endl;
+    //  cout << "last value of num =" << num << endl;
+
+
+    for (int i= 1; i <=5; i++) {
+        cout<<"apna college"<< endl;
+    }
+
+
+    int n ;
+    cout << "enter your n : ";
+    cin>> n;
+
+    for (int i=1; i <=n; i++){
+      cout<< i <<" ";
+    }
+    cout<<endl;
+   
     return 0;
 }
