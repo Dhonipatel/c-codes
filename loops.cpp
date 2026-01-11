b6b3c6a2a6fc14aab1e54a2c8e425d31bb0df99e
Dhoni Patel <iamdhonni@gmail.com>
2026-01-11 13:06:52 +0530

    change

--- loops.cpp
+++ loops.cpp
@@ -1,33 +1,66 @@
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+
+//     // FOR LOOPS
+
+//     // int num;
+
+//     //  for(num = 1; num >=0; num++) {
+//     //     cout<< num<< " ";
+//     //  }
+
+//     //  cout<< endl;
+//     //  cout << "last value of num =" << num << endl;
+
+// //   for (int i= 1; i <=5; i++) {
+// //         cout<<"apna college"<< endl;
+// //     }
+
+// int n;
+// cout<<"enter you n :";
+// cin>> n;
+
+// int sum = 0;
+
+// for (int i = 1; i<=n; i++) {
+//     sum +=i;
+// }
+// cout<<"sum = " << sum<<endl;
+
+//     return 0;
+// };
+
+
 #include <iostream>
 using namespace std;
 
-int main() {
+int main () {
 
-    // FOR LOOPS
+    // int i = 1;
 
-    // int num;
+    // while(i < 3) {
+    //     cout<< i <<" ";
+    //     i++;
+    // }
 
-    //  for(num = 1; num >=0; num++) {
-    //     cout<< num<< " ";
-    //  }
-
-    //  cout<< endl;
-    //  cout << "last value of num =" << num << endl;
-
-
-    for (int i= 1; i <=5; i++) {
-        cout<<"apna college"<< endl;
-    }
-
+    // int i = 1;
+    // while (i <= 10) {
+    //     cout<<"DHONI KUMAR" << " " ;
+    //     i++;
+    // }
 
     int n ;
-    cout << "enter your n : ";
-    cin>> n;
+    cout<< "enter your n = " ;
+    cin >> n;
 
-    for (int i=1; i <=n; i++){
-      cout<< i <<" ";
+    int sum = 0;
+    int i = 1;
+    while(i <= n) {
+       sum +=i;
+       cout << i << " ";
+       i++;
     }
-    cout<<endl;
-   
-    return 0;
+    cout<< "sum = " << sum<<endl;
 }
