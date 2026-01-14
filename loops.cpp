45e8b8c03d5924d58d49a7a4070854262cb48ace
Dhoni Patel <iamdhonni@gmail.com>
2026-01-14 21:06:09 +0530

    change

--- loops.cpp
+++ loops.cpp
@@ -293,10 +293,26 @@ using namespace std;
 int main () {
     
 
-    int n = 7;
+    // int n = 7;
+    // int isPrime = true;
+
+    // for(int i=2; i<=n-1; i++) {
+    //     if(n%i == 0) { //i is a factor n; i comletely divides n; n is non-prime
+    //         isPrime = false;
+    //         break;
+    //     }
+    // }
+
+    // if(isPrime) {
+    //     cout<<"number is prime" <<endl;
+    // } else {
+    //     cout<<"number is not prime" <<endl;
+    // }
+
+   int n = 7;
     int isPrime = true;
 
-    for(int i=2; i<=n-1; i++) {
+    for(int i=2; i<=sqrt(n); i++) {
         if(n%i == 0) { //i is a factor n; i comletely divides n; n is non-prime
             isPrime = false;
             break;
