cb9972a1d3c88c91bc5afbe5d321e6e24f1e2975
Dhoni Patel <iamdhonni@gmail.com>
2026-01-20 21:27:33 +0530

    change

--- loops.cpp
+++ loops.cpp
@@ -71,6 +71,22 @@
 
 //     return 0;
 
+#include <iostream>
+using namespace std;
+
+int main() {
+    int age;
+    cin >> age;
+
+    if(age >= 18)
+        cout << "You can vote";
+    else
+        cout << "You cannot vote";
+
+    return 0;
+}
+
+
 
 
 
