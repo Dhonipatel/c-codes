83a605c5c354c02f3143a61d10ece764104463fb
Dhoni Patel <iamdhonni@gmail.com>
2026-01-21 21:18:18 +0530

    change

--- loops.cpp
+++ loops.cpp
@@ -408,14 +408,37 @@ cout<< answer;
 
 
 
-    return 0;
-
-
-
-
-
+   return 0;
 }
 
+
+
+
+
+
+#include <iostream>
+using namespace std;
+
+int main () {
+
+
+    for(int i=1;i<=10;i++)
+    cout << 5*i << endl;
+
+    int n=2;
+    int sum = 0;
+
+    while(n>0){
+    int d = n%10;
+    sum += d;
+    n/=10;
+}
+
+
+
+
+       return 0;
+}
 
 
 
