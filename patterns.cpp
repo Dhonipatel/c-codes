95938dfc05caad7842a8dcc4dc1ad73d6144a9e2
Dhoni Patel <iamdhonni@gmail.com>
2026-01-27 21:59:50 +0530

    chnage

--- patterns.cpp
+++ patterns.cpp
@@ -107,3 +107,30 @@ int main() {
     return 0;
 }
 
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[] = {2, 7, 11, 15};
+    int n = 4;
+    int target = 9;
+    bool found = false;
+
+    for(int i = 0; i < n; i++) {
+        for(int j = i + 1; j < n; j++) {
+            if(arr[i] + arr[j] == target) {
+                cout << "Pair found: " << arr[i] << " + " << arr[j];
+                found = true;
+                break;
+            }
+        }
+        if(found) break;
+    }
+
+    if(!found)
+        cout << "No pair found";
+
+    return 0;
+}
+
