2cdf0a92618d1a3949a082da3f43c0a4c3aae51e
Dhoni Patel <iamdhonni@gmail.com>
2026-01-28 21:06:48 +0530

    change

--- patterns.cpp
+++ patterns.cpp
@@ -1,145 +1,18 @@
-// #include <iostream>
-// using namespace std;
+#include <iostream>
+using namespace std;
 
-// int main (){
-
-// for(int i = 1; i <= 3; i++) {        // Outer loop
-//     for(int j = 1; j <= 2; j++) {    // Inner loop
-//         cout << i << " " << j << endl;
-//     }
-// }
-
-// for(int i = 1; i <= 3; i++) {
-//     for(int j = 1; j <= 3; j++) {
-//         cout << "* ";
-//     }
-//     cout << endl;
-// }
-
-
-// //   return 0;
-
-// // }
-
-
-
-
-// LIVE SESSION Qs 
-
-// #include <iostream>
-// using namespace std;
-
-// int main () {
-//   // int b = 5;
-//   // int c = --b;
-
-//   // int x = 'a' + b++ - c;
- 
-//   // cout<<b<< " " <<c<<" "<<x<<endl;
-
-//   int b = 5;
-//   int x = ++b + b++;
-//   cout<<b<< " " <<x<<endl;
-
-
-
-  
-// // calculator
-
-//     if (op == '+') {
-//         cout << "a + b =" << (a+b) << endl;
-//     }else if (op == '-') {
-//         cout << " a - b ="<< (a-b)<< endl;
-//     }else if ( op == '*') { 
-//         cout<< " a * b = " << (a*b) << endl;
-//     } else if (op == '/') {
-//         cout<< "a / b =" << ( a/b) << endl;
-//     } else{
-//         cout << "Invalid operator";
-//     }
-
-
-
-// int n = 4;
-// for(int i = 1; i <= n; i++) {
-//     // spaces
-//     for(int s = 1; s <= n - i; s++) {
-//         cout << " ";
-//     }
-//     // stars
-//     for(int j = 1; j <= i; j++) {
-//         cout << "* ";
-//     }
-//     cout << endl;
-// }
-
-
-
-// int n = 4;
-// int num = 1;
-// for(int i = 1; i <= n; i++) {
-//     for(int j = 1; j <= i; j++) {
-//         cout << num << " ";
-//         num++;
-//     }
-//     cout << endl;
-// }
-
-
-
-
-//     return 0;
-// }
-
-// #include <iostream>
-// using namespace std;
-
-// int main() {
-//     int arr[5] = {2, 9, 1, 7, 5};
-//     int max = arr[0];
-
-//     for(int i = 1; i < 5; i++) {
-//         if(arr[i] > max)
-//             max = arr[i];
-//     }
-
-//     cout << "Largest = " << max;
-//     return 0;
-// }
-
-
-// #include <iostream>
-// using namespace std;
-
-// int main() {
-//     int arr[] = {2, 7, 11, 15};
-//     int n = 4;
-//     int target = 9;
-//     bool found = false;
-
-//     for(int i = 0; i < n; i++) {
-//         for(int j = i + 1; j < n; j++) {
-//             if(arr[i] + arr[j] == target) {
-//                 cout << "Pair found: " << arr[i] << " + " << arr[j];
-//                 found = true;
-//                 break;
-//             }
-//         }
-//         if(found) break;
-//     }
-
-//     if(!found)
-//         cout << "No pair found";
-
-//     return 0;
-// }
-
-
-int n = 4;
-for(int i = 1; i <= n; i++) {
-    for(int j = 1; j <= i; j++) {
-        cout << "* ";
+int main() {
+    
+    int n= 5;
+    // outerloop
+    for (int i = 1; i<=n; i++) {
+        // inner loop
+        for (int j = 1; j<=n; j++) {
+            //work
+            cout<<i<< " " ;
+        }
+        cout<<endl;
+        
     }
-    cout << endl;
+    return 0;
 }
-
