6586db754641a5160731274005a9da04f7841d44
Dhoni Patel <iamdhonni@gmail.com>
2026-01-29 21:43:53 +0530

    change

--- patterns.cpp
+++ patterns.cpp
@@ -15,4 +15,13 @@ int main() {
         
     }
     return 0;
+
+    int n = 4;
+for(int i = 1; i <= n; i++) {
+    for(int j = 1; j <= i; j++) {
+        cout << i << " ";
+    }
+    cout << endl;
+}
+
 }
