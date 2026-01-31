ee6a733008da37ce25ef8f13b5b18f604e8f1557
Dhoni Patel <iamdhonni@gmail.com>
2026-01-31 07:46:33 +0530

    change

--- patterns.cpp
+++ patterns.cpp
@@ -3,17 +3,31 @@ using namespace std;
 
 int main() {
     
-    int n= 5;
-    // outerloop
-    for (int i = 1; i<=n; i++) {
-        // inner loop
-       for(int j= 1; j<=n; j++) {
-        cout<<i << " ";
+    // int n= 5;
+    // // outerloop
+    // for (int i = 1; i<=n; i++) {
+    //     // inner loop
+    //    for(int j= 1; j<=n; j++) {
+    //     cout<<i << " ";
+
+    //    }
+    //     cout<<endl;
         
-       }
+    // }
+
+    // PRINT STAR PATTERN
+
+    int n =5;
+
+    for (int i =1; i<=n; i++) {
+        for(int j=1; j<=i; j++) {
+            cout<<"*";
+        }
         cout<<endl;
-        
     }
+
+
+    
     return 0;
 
 
