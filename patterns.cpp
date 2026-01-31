87359492d400d20f8823776b823a63dc349ee8ac
Dhoni Patel <iamdhonni@gmail.com>
2026-01-31 08:19:15 +0530

    change

--- patterns.cpp
+++ patterns.cpp
@@ -17,17 +17,28 @@ int main() {
 
     // PRINT STAR PATTERN
 
-    int n =5;
+    // int n =5;
 
-    for (int i =1; i<=n; i++) {
-        for(int j=1; j<=i; j++) {
-            cout<<"*";
-        }
-        cout<<endl;
+    // for (int i =1; i<=n; i++) {
+    //     for(int j=1; j<=i; j++) {
+    //         cout<<"*";
+    //     }
+    //     cout<<endl;
+    // }
+
+// PRINT INVERTED STAR PETTERN
+
+int n=5;
+
+for(int i =1; i<=n; i++) {
+    for(int j=1; j<=(n-i+1); j++) {
+        cout<<"* ";
     }
+    cout<<endl;
+}
 
+   
 
-    
     return 0;
 
 
