0030717de61bb8cba35b612b2e235df22cdbd877
Dhoni Patel <iamdhonni@gmail.com>
2026-01-31 07:03:30 +0530

    change

--- patterns.cpp
+++ patterns.cpp
@@ -7,21 +7,14 @@ int main() {
     // outerloop
     for (int i = 1; i<=n; i++) {
         // inner loop
-        for (int j = 1; j<=n; j++) {
-            //work
-            cout<<i<< " " ;
-        }
+       for(int j= 1; j<=n; j++) {
+        cout<<i << " ";
+        
+       }
         cout<<endl;
         
     }
     return 0;
 
-    int n = 4;
-for(int i = 1; i <= n; i++) {
-    for(int j = 1; j <= i; j++) {
-        cout << i << " ";
-    }
-    cout << endl;
-}
 
 }
