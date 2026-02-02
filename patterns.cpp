2e0121ca5d804c9693ddfd57cada72c5b6037d44
Dhoni Patel <iamdhonni@gmail.com>
2026-02-02 09:33:42 +0530

    change

--- patterns.cpp
+++ patterns.cpp
@@ -92,17 +92,50 @@ int main() {
 // }
 
 
-int n=4;
-int num=1;
+// int n=4;
+// int num=1;
 
-for(int i=1; i<=n; i++) {
-    for(int j=1; j<=i; j++) {
-        cout<<num++ <<" ";
+// for(int i=1; i<=n; i++) {
+//     for(int j=1; j<=i; j++) {
+//         cout<<num++ <<" ";
      
+//     }
+//     cout<<endl;
+// }
+
+
+int n=4;
+
+// FIRST PYRAMID
+ for(int i=1; i<=n; i++){
+    // space
+    for(int j=1; j<=n-i; j++) {
+        cout<<" ";
+    }
+    for(int j=1; j<=2*i-1; j++) {
+        cout<<"*";
     }
     cout<<endl;
-}
+ }
 
+    // 2ND PYRAMID
+    for(int i=n; i>=1; i--) {
+        //space
+        for(int j=1; j<=n-i; j++) {
+            cout<<" ";
+
+        }
+        // stars
+        for(int j=1; j<= 2*i-1; j++) {
+            cout << "*";
+        }
+        cout<< endl;
+    }
+
+    
+ 
+
+ 
 
 
     return 0;
