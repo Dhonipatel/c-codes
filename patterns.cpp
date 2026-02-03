4450feecb4db81ecfa9bf8a9e24c1ac925c1fd16
Dhoni Patel <iamdhonni@gmail.com>
2026-02-03 08:24:49 +0530

    Update patterns.cpp

--- patterns.cpp
+++ patterns.cpp
@@ -148,16 +148,23 @@ int main() {
  
 int n=4;
 
+// for(int i=1; i<=n; i++) {
+//     for(int j=1; j<=n-i; j++) {
+//         cout<<" ";
+//     }
+//     for(int j=1; j<=i; j++){
+//         cout<<"*";
+//     }
+//     cout<<endl;
+
+
+// }
+
+
 for(int i=1; i<=n; i++) {
-    for(int j=1; j<=n-i; j++) {
-        cout<<" ";
+    for(int j=1; i<=i; j++){
+        
     }
-    for(int j=1; j<=i; j++){
-        cout<<"*";
-    }
-    cout<<endl;
-
-
 }
  
 
