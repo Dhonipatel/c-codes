2bd6eb50161e3299553585f851b997046ae73ad0
Dhoni Patel <iamdhonni@gmail.com>
2026-02-04 07:28:47 +0530

    change

---
+++ 6functions.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+void sayHello() {
+    cout<<"hello :) \n"; 
+}
+
+void assistant() {
+    sayHello();
+    cout<<"work done \n";
+}
+
+int main() {
+
+    assistant();
+
+
+
+    return 0;
+}
