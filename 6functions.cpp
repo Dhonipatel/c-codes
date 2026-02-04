850519166bb0e2c85da7b6a46abff3c648e1f653
Dhoni Patel <iamdhonni@gmail.com>
2026-02-04 08:09:19 +0530

    change

--- 6functions.cpp
+++ 6functions.cpp
@@ -1,20 +1,47 @@
+// #include <iostream>
+// using namespace std;
+
+// void sayHello() {
+//     cout<<"hello :) \n"; 
+// }
+
+// void assistant() {
+//     sayHello();
+//     cout<<"work done \n";
+// }
+
+// int main() {
+
+//     assistant();
+
+
+
+//     return 0;
+// }
+
+
 #include <iostream>
 using namespace std;
 
-void sayHello() {
-    cout<<"hello :) \n"; 
+// int sum (int a , int b) {  //a, b are parameters
+//     int sum = a + b;
+//     return sum;
+// }
+
+
+int diff (int a , int b=1) {  //a, b are parameters
+    int diff = a - b;
+    return diff;
 }
 
-void assistant() {
-    sayHello();
-    cout<<"work done \n";
-}
+int main (){
 
-int main() {
+// int s=  sum (2, 4); // 2,4 are arguments
+// cout<<"sum = " <<s<<endl;
 
-    assistant();
-
-
+int d=  diff (2); // 2,4 are arguments
+cout<<"sum = " <<d<<endl;
 
     return 0;
+
 }
