27f0083307bdbfc7c7d44f8f8833acf08384bab5
Dhoni Patel <iamdhonni@gmail.com>
2026-02-04 08:36:23 +0530

    change

--- 6functions.cpp
+++ 6functions.cpp
@@ -20,28 +20,63 @@
 // }
 
 
-#include <iostream>
-using namespace std;
+// #include <iostream>
+// using namespace std;
 
-// int sum (int a , int b) {  //a, b are parameters
-//     int sum = a + b;
-//     return sum;
+// // int sum (int a , int b) {  //a, b are parameters
+// //     int sum = a + b;
+// //     return sum;
+// // }
+
+
+// int diff (int a , int b=1) {  //a, b are parameters
+//     int diff = a - b;
+//     return diff;
+// }
+
+// int main (){
+
+// // int s=  sum (2, 4); // 2,4 are arguments
+// // cout<<"sum = " <<s<<endl;
+
+// int d=  diff (2); // 2,4 are arguments
+// cout<<"sum = " <<d<<endl;
+
+//     return 0;
+
 // }
 
 
-int diff (int a , int b=1) {  //a, b are parameters
-    int diff = a - b;
-    return diff;
+
+#include <iostream>
+using namespace std;
+
+int mul (int a, int b  ) {
+    int mul = a * b;
+    return mul;
+
 }
 
-int main (){
+// even --> true odd --> false
 
-// int s=  sum (2, 4); // 2,4 are arguments
-// cout<<"sum = " <<s<<endl;
+int isEven(int n) {
+    if(n %2 ==0) {
+        return true;
+    } else{
+        return false;
+    }
+}
 
-int d=  diff (2); // 2,4 are arguments
-cout<<"sum = " <<d<<endl;
+
+
+int main () {
+
+int m = mul(4 , 5);// 4 5 are argumernts
+cout<< "mul  " <<m << endl;
+
+cout<< isEven(6);
 
     return 0;
-
 }
+
+
