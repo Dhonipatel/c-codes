3a46a760ee19eb1822918fe441df218fde50c13c
Dhoni Patel <iamdhonni@gmail.com>
2026-02-11 08:44:29 +0530

    change

--- 7binarynum.cpp
+++ 7binarynum.cpp
@@ -17,9 +17,30 @@ void binToDec(int binNum) {
     cout << decNum <<endl;
 }
 
+void decToBin (int decNum) {
+    int n = decNum;
+    int pow = 1;
+    int binNum = 0;
+
+    while( n > 0) {
+        int rem = n % 2;
+        binNum += rem *pow;
+
+        n = n/2;
+
+        pow = pow *10;
+    } 
+     cout << "binary of " << decNum <<" = " << binNum <<endl;
+}
+
+
+
 int main() {
+    decToBin(37);
 
     binToDec(101);
+
+
 
     // cout << sizeof(int) << endl;
     // cout<< sizeof(long int ) << endl;
