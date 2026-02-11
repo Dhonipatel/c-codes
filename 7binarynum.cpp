502ddfcb038a7413c0053cb1d1c6c7a9d500b5d4
Dhoni Patel <iamdhonni@gmail.com>
2026-02-11 08:23:01 +0530

    change

--- 7binarynum.cpp
+++ 7binarynum.cpp
@@ -6,8 +6,13 @@ int main() {
     // cout << sizeof(int) << endl;
     // cout<< sizeof(long int ) << endl;
 
-    cout<< sizeof(double ) << endl;
-    cout<< sizeof(long double ) << endl;
+    // cout<< sizeof(double ) << endl;
+    // cout<< sizeof(long double ) << endl;
+
+    //   cout<< sizeof(int ) << endl;
+    // cout<< sizeof(short int ) << endl;
+
+    // cout<< sizeof(long long ) << endl;
 
     return 0;
 }
