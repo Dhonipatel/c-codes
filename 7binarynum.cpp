00e33e273fbc9902539b492994062ff934450527
Dhoni Patel <iamdhonni@gmail.com>
2026-02-12 21:29:58 +0530

    change

--- 7binarynum.cpp
+++ 7binarynum.cpp
@@ -1,57 +1,78 @@
-#include <iostream> 
+// #include <iostream> 
+// using namespace std;
+
+
+// void binToDec(int binNum) {
+//     int n= binNum ;
+//     int decNum =0;
+//     int pow = 1;
+
+//     while(n> 0) {
+//         int lastDig = n %10;
+//         decNum += lastDig *pow;
+//         pow = pow *2;
+//         n = n/10;
+//     }
+
+//     cout << decNum <<endl;
+// }
+
+// void decToBin (int decNum) {
+//     int n = decNum;
+//     int pow = 1;
+//     int binNum = 0;
+
+//     while( n > 0) {
+//         int rem = n % 2;
+//         binNum += rem *pow;
+
+//         n = n/2;
+
+//         pow = pow *10;
+//     } 
+//      cout << "binary of " << decNum <<" = " << binNum <<endl;
+// }
+
+
+
+// int main() {
+//     decToBin(37);
+
+//     binToDec(101);
+
+
+
+//     // cout << sizeof(int) << endl;
+//     // cout<< sizeof(long int ) << endl;
+
+//     // cout<< sizeof(double ) << endl;
+//     // cout<< sizeof(long double ) << endl;
+
+//     //   cout<< sizeof(int ) << endl;
+//     // cout<< sizeof(short int ) << endl;
+
+//     // cout<< sizeof(long long ) << endl;
+
+//     return 0;
+// }
+
+
+
+#include <unordered_map>
 using namespace std;
 
+vector<int> twoSum(vector<int>& nums, int target) {
+    unordered_map<int, int> mp;
 
-void binToDec(int binNum) {
-    int n= binNum ;
-    int decNum =0;
-    int pow = 1;
+    for(int i = 0; i < nums.size(); i++) {
+        int need = target - nums[i];
 
-    while(n> 0) {
-        int lastDig = n %10;
-        decNum += lastDig *pow;
-        pow = pow *2;
-        n = n/10;
+        if(mp.find(need) != mp.end()) {
+            return {mp[need], i};
+        }
+
+        mp[nums[i]] = i;
     }
 
-    cout << decNum <<endl;
-}
-
-void decToBin (int decNum) {
-    int n = decNum;
-    int pow = 1;
-    int binNum = 0;
-
-    while( n > 0) {
-        int rem = n % 2;
-        binNum += rem *pow;
-
-        n = n/2;
-
-        pow = pow *10;
-    } 
-     cout << "binary of " << decNum <<" = " << binNum <<endl;
-}
-
-
-
-int main() {
-    decToBin(37);
-
-    binToDec(101);
-
-
-
-    // cout << sizeof(int) << endl;
-    // cout<< sizeof(long int ) << endl;
-
-    // cout<< sizeof(double ) << endl;
-    // cout<< sizeof(long double ) << endl;
-
-    //   cout<< sizeof(int ) << endl;
-    // cout<< sizeof(short int ) << endl;
-
-    // cout<< sizeof(long long ) << endl;
-
-    return 0;
+    return {};
 }
