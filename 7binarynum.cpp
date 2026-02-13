f5b40f73fffd1b2e3dcfb6e27615597c298f3f31
Dhoni Patel <iamdhonni@gmail.com>
2026-02-13 21:00:16 +0530

    change

--- 7binarynum.cpp
+++ 7binarynum.cpp
@@ -82,9 +82,27 @@ int num = binToDec(binary_num);
 
 cout<<num;
 
+
+
+
+int n = 4;
+int num = 1;
+for(int i = 1; i <= n; i++) {
+    for(int j = 1; j <= i; j++) {
+        cout << num << " ";
+        num++;
+    }
+    cout << endl;
+}
+
+
 return 0;
 
 }
+
+
+
+
 
 
 
