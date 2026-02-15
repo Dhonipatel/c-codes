ba54470d99edbef20682d79dcff008c2b8b4a7d7
Dhoni Patel <iamdhonni@gmail.com>
2026-02-15 21:09:39 +0530

    chnage

--- 7binarynum.cpp
+++ 7binarynum.cpp
@@ -103,6 +103,20 @@ int num = 13;
 string binaryNumber = decimalToBinary(num);
  cout<<binaryNumber;
 
+
+
+
+ int n = 4;
+int num = 1;
+for(int i = 1; i <= n; i++) {
+    for(int j = 1; j <= i; j++) {
+        cout << num << " ";
+        num++;
+    }
+    cout << endl;
+}
+
+
 }
 
 
