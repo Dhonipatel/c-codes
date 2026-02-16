20b1995a7e740cfacb42e59fc251b37009c71847
Dhoni Patel <iamdhonni@gmail.com>
2026-02-16 08:32:02 +0530

    change

--- 8pointer.cpp
+++ 8pointer.cpp
@@ -1,17 +1,48 @@
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+ 
+//     int a =10;
+//     int *ptr= &a;
+
+//     // float pi = 3.14;
+//     // float *ptr2 = &pi;
+
+
+//     int **pptr = &ptr;
+//     cout << &ptr << " = " << pptr << "\n";
+
+
+
+
+
+//     // cout<<&pi <<" = " <<ptr2 << "\n";
+
+//     // cout<<&a << " = " << ptr << "\n";
+
+//     return 0;
+// }
+
+
+
 #include <iostream>
 using namespace std;
 
-int main() {
- 
-    int a =10;
-    int *ptr= &a;
+int main () {
 
-    float pi = 3.14;
-    float *ptr2 = &pi;
+    int a = 10;
+    int *ptr = &a;
 
-    cout<<&pi <<" = " <<ptr2 << "\n";
+    cout << ptr <<"\n";
+    cout << *ptr <<"\n";
 
-    cout<<&a << " = " << ptr << "\n";
+    *ptr = 20;
+
+    cout << a <<"\n";
+    
+
 
     return 0;
 }
+
