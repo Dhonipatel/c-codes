26f22f50ababf674be599d26fcaa4cd2ef65c087
Dhoni Patel <iamdhonni@gmail.com>
2026-02-16 08:07:17 +0530

    change

--- 8pointer.cpp
+++ 8pointer.cpp
@@ -2,10 +2,16 @@
 using namespace std;
 
 int main() {
-
+ 
     int a =10;
+    int *ptr= &a;
 
-    cout<<&a <<"/n";
+    float pi = 3.14;
+    float *ptr2 = &pi;
+
+    cout<<&pi <<" = " <<ptr2 << "\n";
+
+    cout<<&a << " = " << ptr << "\n";
 
     return 0;
 }
