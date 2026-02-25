7d146c34f664e58d48de7994480dd0945de34fdf
Dhoni Patel <iamdhonni@gmail.com>
2026-02-25 08:02:31 +0530

    change

--- 9Arrays.cpp
+++ 9Arrays.cpp
@@ -4,8 +4,23 @@ using namespace std;
 int main () {
 
 
-   int arr[5] = {7, 5, 2, 1, 3};
+//    int arr[5] = {7, 5, 2, 1, 3};
+//    int n = sizeof(arr) / sizeof(int);
+
+//    for(int i =0; i<n; i++) {
+//     cout <<arr[i] <<" "; // idx = 0, 1, 2, 3, 4,
+
+//    }
+//    cout << endl;
+
+
+
+ int arr[5] ;
    int n = sizeof(arr) / sizeof(int);
+
+   for(int i=0; i<n; i++) {
+    cin>>arr[i];
+   }
 
    for(int i =0; i<n; i++) {
     cout <<arr[i] <<" "; // idx = 0, 1, 2, 3, 4,
