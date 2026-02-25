2e53997f51f0476466e2ffb42bf97e27fbee122e
Dhoni Patel <iamdhonni@gmail.com>
2026-02-25 08:30:10 +0530

    change

--- 9Arrays.cpp
+++ 9Arrays.cpp
@@ -30,19 +30,34 @@ int main () {
 
 
 
-   int n;
-   cout<<"enter your length of array :";
+//    int n;
+//    cout<<"enter your length of array :";
 
-   cin>>n;
+//    cin>>n;
 
-   int arr[n];
-   for(int i=0; i<n; i++) {
-    cin>>arr[i];
-   }
+//    int arr[n];
+//    for(int i=0; i<n; i++) {
+//     cin>>arr[i];
+//    }
 
-   for(int i=0; i<n; i++ ) {
-    cout<<arr[i] <<",";
-   }
+//    for(int i=0; i<n; i++ ) {
+//     cout<<arr[i] <<",";
+//    }
+
+
+int arr [] = {5, 4, 3, 9 ,12};
+int n= sizeof(arr) /sizeof(int);
+
+int max = arr [0];
+for(int i = 0; i<n; i++){
+    if(arr[i] > max) {
+        max= arr[i];
+    }
+}
+cout <<"lagest = " <<max <<endl;
+
+
+
 
     return 0;
 }
