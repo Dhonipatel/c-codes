1b129ee512ec3b828d1d5ff214a2e0ab54db07c1
Dhoni Patel <iamdhonni@gmail.com>
2026-02-26 20:13:57 +0530

    change

--- 9Arrays.cpp
+++ 9Arrays.cpp
@@ -78,6 +78,20 @@ using namespace std;
 
 int  main() {
 
+    int arr [] = {5, 4, 3, 9 ,12};
+
+    int n =sizeof(arr) / sizeof(int);
+
+    int max=arr[0];
+
+    for(int i = 0; i<n; i++) {
+        if(arr[i] > max) {
+         max = arr[i];
+
+        }
+
+    }
+    cout<<"lergest = "<<max<< endl;
 
     return 0;
 }
