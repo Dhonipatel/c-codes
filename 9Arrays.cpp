8e6ca0c0e2e53f6bd5bd7eb1d855bb921803b999
Dhoni Patel <iamdhonni@gmail.com>
2026-02-26 07:54:50 +0530

    change

--- 9Arrays.cpp
+++ 9Arrays.cpp
@@ -1,71 +1,82 @@
+// #include <iostream>
+// using namespace std;
+
+// int main () {
+
+
+// //    int arr[5] = {7, 5, 2, 1, 3};
+// //    int n = sizeof(arr) / sizeof(int);
+
+// //    for(int i =0; i<n; i++) {
+// //     cout <<arr[i] <<" "; // idx = 0, 1, 2, 3, 4,
+
+// //    }
+// //    cout << endl;
+
+
+
+// //  int arr[5] ;
+// //    int n = sizeof(arr) / sizeof(int);
+
+// //    for(int i=0; i<n; i++) {
+// //     cin>>arr[i];
+// //    }
+
+// //    for(int i =0; i<n; i++) {
+// //     cout <<arr[i] <<" "; // idx = 0, 1, 2, 3, 4,
+
+// //    }
+// //    cout << endl;
+
+
+
+// //    int n;
+// //    cout<<"enter your length of array :";
+
+// //    cin>>n;
+
+// //    int arr[n];
+// //    for(int i=0; i<n; i++) {
+// //     cin>>arr[i];
+// //    }
+
+// //    for(int i=0; i<n; i++ ) {
+// //     cout<<arr[i] <<",";
+// //    }
+
+
+// // int arr [] = {5, 4, 3, 9 ,12};
+// // int n= sizeof(arr) /sizeof(int);
+
+// // int max = arr [0];
+// // for(int i = 0; i<n; i++){
+// //     if(arr[i] > max) {
+// //         max= arr[i];
+// //     }
+// // }
+// // cout <<"lagest = " <<max <<endl;
+
+// int arr[]= {3, 5, 6,2,7};
+// int n= sizeof(arr) / sizeof(int);
+
+// int small  =arr[0];
+// for(int i=0; i<n; i++) {
+//     if(arr[i] < small) {
+//         small= arr[i];
+//     }
+// }
+// cout<<"smallest value = " << small <<endl;
+
+
+
+//     return 0;
+// }
+
+
 #include <iostream>
 using namespace std;
 
-int main () {
-
-
-//    int arr[5] = {7, 5, 2, 1, 3};
-//    int n = sizeof(arr) / sizeof(int);
-
-//    for(int i =0; i<n; i++) {
-//     cout <<arr[i] <<" "; // idx = 0, 1, 2, 3, 4,
-
-//    }
-//    cout << endl;
-
-
-
-//  int arr[5] ;
-//    int n = sizeof(arr) / sizeof(int);
-
-//    for(int i=0; i<n; i++) {
-//     cin>>arr[i];
-//    }
-
-//    for(int i =0; i<n; i++) {
-//     cout <<arr[i] <<" "; // idx = 0, 1, 2, 3, 4,
-
-//    }
-//    cout << endl;
-
-
-
-//    int n;
-//    cout<<"enter your length of array :";
-
-//    cin>>n;
-
-//    int arr[n];
-//    for(int i=0; i<n; i++) {
-//     cin>>arr[i];
-//    }
-
-//    for(int i=0; i<n; i++ ) {
-//     cout<<arr[i] <<",";
-//    }
-
-
-// int arr [] = {5, 4, 3, 9 ,12};
-// int n= sizeof(arr) /sizeof(int);
-
-// int max = arr [0];
-// for(int i = 0; i<n; i++){
-//     if(arr[i] > max) {
-//         max= arr[i];
-//     }
-// }
-// cout <<"lagest = " <<max <<endl;
-
-int arr[]= {3, 5, 6,2,7};
-int n= sizeof(arr) / sizeof(int);
-
-int small  =arr[0];
-for(int i=0; i<n; i++) {
-    if(arr[i] < small) {
-        small= arr[i];
-    }
-}
-cout<<"smallest value = " << small <<endl;
+int  main() {
 
 
     return 0;
