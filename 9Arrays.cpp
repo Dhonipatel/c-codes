37ce2066ab9c08c0983a98ebf894d234de755cfa
Dhoni Patel <iamdhonni@gmail.com>
2026-02-27 07:20:49 +0530

    change

--- 9Arrays.cpp
+++ 9Arrays.cpp
@@ -73,25 +73,49 @@
 // }
 
 
+// #include <iostream>
+// using namespace std;
+
+// int  main() {
+
+//     int arr [] = {5, 4, 3, 9 ,12};
+
+//     int n =sizeof(arr) / sizeof(int);
+
+//     int max=arr[0];
+
+//     for(int i = 0; i<n; i++) {
+//         if(arr[i] > max) {
+//          max = arr[i];
+
+//         }
+
+//     }
+//     cout<<"lergest = "<<max<< endl;
+
+//     return 0;
+// }
+
+
 #include <iostream>
 using namespace std;
 
+int linearSearch(int *arr, int n, int key) {
+    for(int i=0; i<n; i++) {
+        if(arr[i] == key) {
+            return i;
+        }
+    }
+    return -1;
+}
+
 int  main() {
 
-    int arr [] = {5, 4, 3, 9 ,12};
+    int arr[]= {2, 4, 6, 8, 10, 12, 14, 16};
+    int n= sizeof(arr) /sizeof(int);
 
-    int n =sizeof(arr) / sizeof(int);
+    cout<<  linearSearch(arr, n, 10) <<endl;
 
-    int max=arr[0];
-
-    for(int i = 0; i<n; i++) {
-        if(arr[i] > max) {
-         max = arr[i];
-
-        }
-
-    }
-    cout<<"lergest = "<<max<< endl;
 
     return 0;
 }
