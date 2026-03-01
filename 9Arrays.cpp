99af6568e0e0af5ec133e5721079dbea53245326
Dhoni Patel <iamdhonni@gmail.com>
2026-03-01 08:41:04 +0530

    change

--- 9Arrays.cpp
+++ 9Arrays.cpp
@@ -212,36 +212,69 @@
 // BINARY SEARCH
 /////////////////////
 
+// #include <iostream>
+// using namespace std;
+
+// int binSearch(int *arr, int n, int key) {
+//     int st=0 , end = n-1;
+    
+//     while(st <= end) {
+//         int mid =(st +end) / 2;
+//         if(arr[mid] == key) {
+//             return mid; //key found
+//         } else if(arr[mid] < key) { // 2nd half
+//             st = mid + 1;
+
+//         }else {
+//             // 1st half
+//             end = mid-1;
+//         }
+
+//     }
+//     return -1;
+// }
+
+// int main() {
+
+//     int arr[] = {2, 4, 6, 8, 10, 12, 14, 16,};
+//     int n =sizeof(arr) / sizeof(int);
+
+//     cout << binSearch(arr, n, 12) << endl;
+
+
+
+//     return 0;
+// }
+
+
+////////////////////////
+// POINTER ARITHMETIC
+//////////////////////
+
 #include <iostream>
 using namespace std;
 
-int binSearch(int *arr, int n, int key) {
-    int st=0 , end = n-1;
-    
-    while(st <= end) {
-        int mid =(st +end) / 2;
-        if(arr[mid] == key) {
-            return mid; //key found
-        } else if(arr[mid] < key) { // 2nd half
-            st = mid + 1;
-
-        }else {
-            // 1st half
-            end = mid-1;
-        }
-
-    }
-    return -1;
-}
-
 int main() {
 
-    int arr[] = {2, 4, 6, 8, 10, 12, 14, 16,};
-    int n =sizeof(arr) / sizeof(int);
+    // increament 
 
-    cout << binSearch(arr, n, 12) << endl;
+    // char ch ='a';
+    // int a = 10;
+    // int *ptr = &ch;
+    // cout<< ptr << "\n";
+    // ptr++; //1 char ++
+    // cout << ptr << "\n";
+
+    // decreament
 
 
+    int a = 5;
+    int *ptr = &a;
+
+    cout << ptr << "\n";
+    ptr++;
+    cout << ptr << "\n";
+    ptr--;
 
     return 0;
 }
