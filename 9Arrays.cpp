cf4815143539aa61b50f4ef658c9deeb820d251f
Dhoni Patel <iamdhonni@gmail.com>
2026-03-01 07:58:32 +0530

    change

--- 9Arrays.cpp
+++ 9Arrays.cpp
@@ -218,7 +218,7 @@ using namespace std;
 int binSearch(int *arr, int n, int key) {
     int st=0 , end = n-1;
     
-    while(st <= n) {
+    while(st <= end) {
         int mid =(st +end) / 2;
         if(arr[mid] == key) {
             return mid; //key found
@@ -239,8 +239,8 @@ int main() {
     int arr[] = {2, 4, 6, 8, 10, 12, 14, 16,};
     int n =sizeof(arr) / sizeof(int);
 
-    cout << binSearch(arr, n, 12);
-    
+    cout << binSearch(arr, n, 12) << endl;
+
 
 
     return 0;
