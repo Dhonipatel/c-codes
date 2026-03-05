7e9b432a432c41615fd0fd8da4049b79de876b93
Dhoni Patel <iamdhonni@gmail.com>
2026-03-05 21:36:11 +0530

    change

--- 9Arrays.cpp
+++ 9Arrays.cpp
@@ -307,38 +307,38 @@
 
 
 
-#include <iostream>
-#include <vector>
-using namespace std;
+// #include <iostream>
+// #include <vector>
+// using namespace std;
 
-vector<int> productExceptSelf(vector<int>& nums) {
-    int n = nums.size();
-    vector<int> answer(n, 1);
+// vector<int> productExceptSelf(vector<int>& nums) {
+//     int n = nums.size();
+//     vector<int> answer(n, 1);
 
-    // Step 1: Left products
-    int left = 1;
-    for(int i = 0; i < n; i++) {
-        answer[i] = left;
-        left *= nums[i];
-    }
+//     // Step 1: Left products
+//     int left = 1;
+//     for(int i = 0; i < n; i++) {
+//         answer[i] = left;
+//         left *= nums[i];
+//     }
 
-    // Step 2: Right products
-    int right = 1;
-    for(int i = n - 1; i >= 0; i--) {
-        answer[i] *= right;
-        right *= nums[i];
-    }
+//     // Step 2: Right products
+//     int right = 1;
+//     for(int i = n - 1; i >= 0; i--) {
+//         answer[i] *= right;
+//         right *= nums[i];
+//     }
 
-    return answer;
-}
+//     return answer;
+// }
 
-int main() {
-    vector<int> nums = {1,2,3,4};
-    vector<int> result = productExceptSelf(nums);
+// int main() {
+//     vector<int> nums = {1,2,3,4};
+//     vector<int> result = productExceptSelf(nums);
 
-    for(int num : result) {
-        cout << num << " ";
-    }
+//     for(int num : result) {
+//         cout << num << " ";
+//     }
 
-    return 0;
-}
+//     return 0;
+// }
