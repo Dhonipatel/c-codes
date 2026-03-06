79052ecbca6ffb29b34a2595febca71d02575286
Dhoni Patel <iamdhonni@gmail.com>
2026-03-06 07:37:06 +0530

    change

--- 9Arrays.cpp
+++ 9Arrays.cpp
@@ -251,8 +251,8 @@
 // POINTER ARITHMETIC
 //////////////////////
 
-#include <iostream>
-using namespace std;
+// #include <iostream>
+// using namespace std;
 
 // void printArr(int *ptr, int n) {
 //     for(int i=0; i<n; i++) {
@@ -261,7 +261,7 @@ using namespace std;
 //     }
 // }
 
-int main() {
+// int main() {
 
 //     // increament 
 
@@ -303,21 +303,46 @@ int main() {
 
 // POINTER ARITHMETIC
     
-int arr[20] = {1, 2, 3, 4, 5, 6};
-int *ptr1 = arr;
-int *ptr2 = ptr1 + 3;
+// int arr[20] = {1, 2, 3, 4, 5, 6};
+// int *ptr1 = arr;
+// int *ptr2 = ptr1 + 3;
 
-// subtraction
+// // subtraction
 
-// cout << ptr2 - ptr1 <<"\n";
+// // cout << ptr2 - ptr1 <<"\n";
 
-// compare 
+// // compare 
 
 
-cout <<(ptr2 > ptr1) << "\n"; //yes : true : 1
+// cout <<(ptr2 > ptr1) << "\n"; //yes : true : 1
+
+//     return 0;
+// }
+
+
+#include <iostream>
+using namespace std;
+
+void printSubarrays (int *arr , int n) {
+    for(int start = 0; start < n; start ++) {
+        for (int end=start; end < n; end++) {
+            // cout << "(" << start << "," << end << ")" ;
+
+            for(int i=start; i <= end; i++) {
+                cout << arr[i];
+            }
+            cout<< ", ";
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+
+    int arr[] = {1, 2, 3, 4, 5};
+    int n = 5;
+
+    printSubarrays(arr , n);
 
     return 0;
 }
-
-
-
