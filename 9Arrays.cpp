63141430d0c6a8cf468cb25877b6ac288ea3c91b
Dhoni Patel <iamdhonni@gmail.com>
2026-03-07 07:32:39 +0530

    CHANGE

--- 9Arrays.cpp
+++ 9Arrays.cpp
@@ -360,7 +360,31 @@
 #include <iostream>
 using namespace std;
 
-int main ()  {  
+void maxSubarraySum1(int *arr, int n) {
+  int maxSum = INT _MAX;
 
-        return 0;
+  for(int start=0; start<n; start++) {
+    for(int end=start; end<n; end++) {
+      int currSum =0;
+      for(int i = start; i<=end; i++) {
+        currSum += arr[i];
+      }
+      cout<< currSum << " ,";
+      maxSum = max(maxSum, currSum);
+    }
+
+    cout << endl;
+  }
+  cout <<"maxium subarray sum =" << maxSum << endl;
+
+}
+
+int main ()  { 
+     
+  int arr[] = {2, -3, 6, -5, 4, 2};
+  int n = sizeof(arr)/ sizeof (int);
+
+  maxSubarraySum1(arr , n);
+
+  return 0;
 }
