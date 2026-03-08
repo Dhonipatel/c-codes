ca8d532535fb89db903e45c9f60ac52f08d2f4d0
Dhoni Patel <iamdhonni@gmail.com>
2026-03-08 08:28:25 +0530

    change

--- 9Arrays.cpp
+++ 9Arrays.cpp
@@ -507,22 +507,46 @@
 #include <climits>
 using namespace std;
 
-void trap(int *height, int n) {
-  int leftMax[20000];
-  leftMax[0] = INT_MIN;
+int trap(int *height, int n) {
+  int leftMax[20000], rightMax[20000];
+  leftMax[0] = height[0];
+  rightMax[n-1]= height[n-1];
+
+
+
   for(int i=1; i<n; i++) {
-    leftMax[i]= max(leftMax[i-1], height[i-1]);
-    cout<< leftMax[i] <<" ,";
+    leftMax[i]= max(leftMax[i-1], height[i]);
+    
   }
-  cout <<endl;
+
+  for( int i=n-2; i>=0; i--) {
+    rightMax[i] = max(rightMax[i+1], height[i]);
+  
+  }
+ 
+
+  int waterTrapped = 0;
+  for(int i=0; i<n; i++) {
+    int currWater = min(leftMax[i], rightMax[i]) - height[i];
+    if(currWater > 0) {
+      waterTrapped += currWater;
+    }
+  }
+
+  cout << "water trapped = " <<waterTrapped << endl;
+
+  return waterTrapped;
+
 }
 
 
 
 int main() {
 
-  int height[] = {4, 2, 0, 6, 3, 2, 5};
+  int height[7] = {4, 2, 0, 6, 3, 2, 5};
   int n = sizeof(height) /sizeof(int);
+
+  trap(height, n);
 
   return 0;
 }
