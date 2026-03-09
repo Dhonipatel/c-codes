bbefb44f29da2ba80b74e9b144cf16e1d6da7680
Dhoni Patel <iamdhonni@gmail.com>
2026-03-09 07:17:31 +0530

    change

--- 9Arrays.cpp
+++ 9Arrays.cpp
@@ -501,52 +501,52 @@
 
 
 
-// TRAPPING RAINWATER
+// // TRAPPING RAINWATER
 
-#include <iostream>
-#include <climits>
-using namespace std;
+// #include <iostream>
+// #include <climits>
+// using namespace std;
 
-int trap(int *height, int n) {
-  int leftMax[20000], rightMax[20000];
-  leftMax[0] = height[0];
-  rightMax[n-1]= height[n-1];
+// int trap(int *height, int n) {
+//   int leftMax[20000], rightMax[20000];
+//   leftMax[0] = height[0];
+//   rightMax[n-1]= height[n-1];
 
 
 
-  for(int i=1; i<n; i++) {
-    leftMax[i]= max(leftMax[i-1], height[i]);
+//   for(int i=1; i<n; i++) {
+//     leftMax[i]= max(leftMax[i-1], height[i]);
     
-  }
+//   }
 
-  for( int i=n-2; i>=0; i--) {
-    rightMax[i] = max(rightMax[i+1], height[i]);
+//   for( int i=n-2; i>=0; i--) {
+//     rightMax[i] = max(rightMax[i+1], height[i]);
   
-  }
+//   }
  
 
-  int waterTrapped = 0;
-  for(int i=0; i<n; i++) {
-    int currWater = min(leftMax[i], rightMax[i]) - height[i];
-    if(currWater > 0) {
-      waterTrapped += currWater;
-    }
-  }
+//   int waterTrapped = 0;
+//   for(int i=0; i<n; i++) {
+//     int currWater = min(leftMax[i], rightMax[i]) - height[i];
+//     if(currWater > 0) {
+//       waterTrapped += currWater;
+//     }
+//   }
 
-  cout << "water trapped = " <<waterTrapped << endl;
+//   cout << "water trapped = " <<waterTrapped << endl;
 
-  return waterTrapped;
+//   return waterTrapped;
 
-}
+// }
 
 
 
-int main() {
+// int main() {
 
-  int height[7] = {4, 2, 0, 6, 3, 2, 5};
-  int n = sizeof(height) /sizeof(int);
+//   int height[7] = {4, 2, 0, 6, 3, 2, 5};
+//   int n = sizeof(height) /sizeof(int);
 
-  trap(height, n);
+//   trap(height, n);
 
-  return 0;
-}
+//   return 0;
+// }
