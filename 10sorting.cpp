1e9a07ac78975caa72c7b03b6e5cfd21f8715a1e
Dhoni Patel <iamdhonni@gmail.com>
2026-03-14 08:20:11 +0530

    change

--- 10sorting.cpp
+++ 10sorting.cpp
@@ -1,3 +1,7 @@
+
+// BUBBLE SORT  ////////////////
+
+
 // #include <iostream>
 // using namespace std;
 
@@ -33,41 +37,80 @@
 
 
 
+// #include <iostream>
+// using namespace std;
+
+// void print (int arr[], int n) {
+//     for(int i=0; i<n; i++){
+//         cout << arr[i] <<" ";
+//     }
+// }
+
+//   void bubbleSort(int arr[], int n) {
+    
+//     for(int i=0; i<n-1; i++) {
+//         bool isSwap = false;
+//         cout<<"inner loop";
+//         for(int j=0; j<n-i-1; j++) {
+
+//             if(arr[j] > arr[j+1]) {
+//                 cout << "inner loop";
+//                 swap(arr[j], arr[j+1]);
+//                 isSwap = true;
+//             }
+//         }
+//         if(!isSwap) {
+//             //array is already sorted
+//             return;
+//         }
+//     }
+//     print(arr, n);
+// }
+
+// int main() {
+//     int arr[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+//     bubbleSort(arr , 10);
+//     cout << endl;
+
+
+//     return 0;
+// }
+
+
+
+// SELECTION SORT ////////////
+
 #include <iostream>
 using namespace std;
 
-void print (int arr[], int n) {
-    for(int i=0; i<n; i++){
-        cout << arr[i] <<" ";
+void print(int arr[] , int n){
+    for(int i=0; i<n; i++) {
+        cout<<arr[i] << " ";
     }
+
 }
+void selectionSort(int arr[], int n) {
 
-  void bubbleSort(int arr[], int n) {
-    
-    for(int i=0; i<n-1; i++) {
-        bool isSwap = false;
-        cout<<"inner loop";
-        for(int j=0; j<n-i-1; j++) {
+    for(int i= 0; i<n-1; i++) {
 
-            if(arr[j] > arr[j+1]) {
-                cout << "inner loop";
-                swap(arr[j], arr[j+1]);
-                isSwap = true;
+        int minIdx = i;
+        for(int j=i+1; j<n; j++) {
+            if(arr[j] < arr[minIdx]) {
+
+                minIdx =j;
+
             }
         }
-        if(!isSwap) {
-            //array is already sorted
-            return;
-        }
+        swap(arr[i], arr[minIdx]);
+
     }
-    print(arr, n);
+    print(arr , n);
 }
 
-int main() {
-    int arr[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    bubbleSort(arr , 10);
-    cout << endl;
+int main () {
 
+    int arr[5] ={5, 4, 1, 3 , 2};
+    selectionSort(arr , 5);
 
     return 0;
 }
