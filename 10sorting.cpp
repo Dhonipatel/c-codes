0aa2a48c8b01994df1c10026473f5d3aa5320d83
Dhoni Patel <iamdhonni@gmail.com>
2026-03-14 07:38:00 +0530

    change

--- 10sorting.cpp
+++ 10sorting.cpp
@@ -1,3 +1,38 @@
+// #include <iostream>
+// using namespace std;
+
+// void print (int arr[], int n) {
+//     for(int i=0; i<n; i++){
+//         cout << arr[i] <<" ";
+//     }
+// }
+
+//   void bubbleSort(int arr[], int n) {
+    
+//     for(int i=0; i<n-1; i++) {
+//         for(int j=0; j<n-i-1; j++) {
+
+//             if(arr[j] > arr[j+1]) {
+//                 swap(arr[j], arr[j+1]);
+              
+//             }
+//         }
+//     }
+//     print(arr, n);
+// }
+
+// int main() {
+//     int arr[5] = {5, 4,1, 3, 2};
+//     bubbleSort(arr , 5);
+
+
+//     return 0;
+// }
+
+
+
+
+
 #include <iostream>
 using namespace std;
 
@@ -10,19 +45,28 @@ void print (int arr[], int n) {
   void bubbleSort(int arr[], int n) {
     
     for(int i=0; i<n-1; i++) {
+        bool isSwap = false;
+        cout<<"inner loop";
         for(int j=0; j<n-i-1; j++) {
 
             if(arr[j] > arr[j+1]) {
+                cout << "inner loop";
                 swap(arr[j], arr[j+1]);
+                isSwap = true;
             }
+        }
+        if(!isSwap) {
+            //array is already sorted
+            return;
         }
     }
     print(arr, n);
 }
 
 int main() {
-    int arr[5] = {5, 4,1, 3, 2};
-    bubbleSort(arr , 5);
+    int arr[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    bubbleSort(arr , 10);
+    cout << endl;
 
 
     return 0;
