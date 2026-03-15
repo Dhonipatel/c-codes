763aa974e24633a16a4c3f7985ca5afb697d884b
Dhoni Patel <iamdhonni@gmail.com>
2026-03-15 07:50:58 +0530

    change

--- 10sorting.cpp
+++ 10sorting.cpp
@@ -117,3 +117,50 @@
 
 //     return 0;
 // }
+
+
+// COUNTING SORT //////////
+
+#include <iostream>
+#include <climits>
+using namespace std;
+
+ void print(int arr[], int n) {
+    for(int i=0; i<n; i++) {
+        cout<<arr[i] << " ";
+    }
+}
+
+void countSort(int arr[], int n) {
+    int freq[10000] = {0}; ///range
+    int minVal = INT_MAX,  maxVal = INT_MIN;
+    for(int i=0; i<n; i++) {
+        minVal = min(minVal, arr[i]);
+        maxVal= max(maxVal, arr[i]);
+    }
+
+    //1st STEP - O(n)
+
+    for(int i=0; i<n; i++) {
+        freq[arr[i]]++;
+    }
+
+    //2nd  STEP - O(range) = max - min
+
+    for(int i=minVal, j=0; i<=maxVal; i++) {
+        while(freq[i] > 0) {
+            arr[j++] =i;
+            freq[i]--;
+        }
+    }
+
+    print(arr, n);
+}
+
+int main() {
+    int arr[8] = {1, 4, 1, 3, 2, 4, 3 ,7 };
+    countSort(arr, 8);
+    return 0;
+}
+
+
