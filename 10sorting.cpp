cae01583bcfab8af783ecbc4eaea8a2ca2ceaea7
Dhoni Patel <iamdhonni@gmail.com>
2026-03-18 21:06:51 +0530

    change

--- 10sorting.cpp
+++ 10sorting.cpp
@@ -171,5 +171,31 @@
 
 // LIVE SESSION //////////
 
+#include <iostream>
+#include <climits>
+using namespace std;
 
+int maxSubarraySum(int arr[], int n) {
+    int currSum = 0;
+    int maxSum = INT_MIN;
+
+    for(int i = 0; i < n; i++) {
+        currSum += arr[i];
+
+        maxSum = max(maxSum, currSum);
+
+        if(currSum < 0) {
+            currSum = 0;
+        }
+    }
+
+    return maxSum;
+}
+
+int main() {
+    int arr[] = {-2,1,-3,4,-1,2,1,-5,4};
+    int n = 9;
+
+    cout << "Max Subarray Sum = " << maxSubarraySum(arr,n);
+}
 
