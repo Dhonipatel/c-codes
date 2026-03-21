7d9209b4bf0d6e1e46dd7bdcdcf118969ce094b4
Dhoni Patel <iamdhonni@gmail.com>
2026-03-21 06:53:01 +0530

    change

--- 11,2Darray.cpp
+++ 11,2Darray.cpp
@@ -1,13 +1,25 @@
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+
+//     int  students[3] [3] = {{100, 100, 100},
+//                              {85, 74, 89},
+//                             {63, 72, 65}};
+
+//      cout << students[1] [1] ;                       
+
+//     return 0;
+// }
+
+
+
 #include <iostream>
 using namespace std;
 
 int main() {
-
-    int  students[3] [3] = {{100, 100, 100},
-                             {85, 74, 89},
-                            {63, 72, 65}};
-
-     cout << students[1] [1] ;                       
+    
+    int arr[3][4];
 
     return 0;
 }
