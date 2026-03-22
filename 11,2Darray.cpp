22ceeff7521a0c77220fdc978a7d7da53c26a8b7
Dhoni Patel <iamdhonni@gmail.com>
2026-03-22 08:51:29 +0530

    change

--- 11,2Darray.cpp
+++ 11,2Darray.cpp
@@ -112,3 +112,60 @@
 
 //     return 0;
 // }
+
+
+// DIOGONAL SUM /////////////////////
+
+#include <iostream> 
+using namespace std;
+
+int diagonaSum(int mat [] [3], int n) {
+    int sum =0;
+
+//     for(int i=0; i<n; i++) { // rows
+//         for(int j=0; j<n; j++) { //cols
+//             if(i== j) {
+//                 sum += mat[i][j];
+//             } else if(j == n-i-1) {
+//                 sum += mat[i][j];
+//             }
+
+//         }
+
+//     }
+//     cout << "sum = " << sum << endl;
+//     return sum;
+// }
+
+// O(N)
+
+for(int i=0; i<n; i++) {
+    sum += mat[i][i]; //pd
+    if(i != n-i-1) {
+        sum += mat[i][n-i-1]; // sd
+    }
+}
+
+cout << "sum =" << sum << endl;
+return sum;
+
+}
+
+int main() {
+
+           int matrix[4][4] = {{1, 2, 3, 4},
+                            {5, 6, 7, 8 },
+                            {9, 10, 11, 12},
+                            {13, 14, 15, 16}};
+  
+
+
+    int matrix2 [3][3] = {{1, 2, 3},
+                         {4, 5, 6 },
+                        { 7, 8, 9}};
+    diagonaSum(matrix2, 3);
+
+
+
+    return 0;
+}
