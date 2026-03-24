74e4fb778f8fab91bd2d2fbc0f9c2d3a5a3bb3b5
Dhoni Patel <iamdhonni@gmail.com>
2026-03-24 07:46:24 +0530

    change

--- 11,2Darray.cpp
+++ 11,2Darray.cpp
@@ -116,11 +116,11 @@
 
 // DIOGONAL SUM /////////////////////
 
-#include <iostream> 
-using namespace std;
+// #include <iostream> 
+// using namespace std;
 
-int diagonaSum(int mat [] [3], int n) {
-    int sum =0;
+// int diagonaSum(int mat [] [3], int n) {
+//     int sum =0;
 
 //     for(int i=0; i<n; i++) { // rows
 //         for(int j=0; j<n; j++) { //cols
@@ -171,3 +171,38 @@ int diagonaSum(int mat [] [3], int n) {
 // }
 
 
+#include <iostream>
+using namespace std;
+
+bool search(int mat[][4], int n, int m, int key) {
+    int i =0, j = m-1;
+
+    while(i < n && j >= 0) {
+        if(mat[i][j]== key) {
+            cout<< "found at cell (" << i << " ," << j << ") \n";
+            return 0;
+        } else if(mat[i][j] > key) {
+            //left
+            j--;
+        } else{
+            // down
+            i++;
+        }
+
+    }
+    cout<< "key not found \n";
+    return false;
+}
+
+int main () {
+
+    int matrix[4][4] = {{10, 20, 30, 40},
+                        {15, 25, 35, 45},
+                        {27, 29, 37, 48},
+                        {32, 33, 39, 50}};
+                        
+    search(matrix, 4, 4, 50);                    
+
+
+    return 0;
+}
