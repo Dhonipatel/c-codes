65f6d39d150a485ebe87db8f000597a8fe3d3383
Dhoni Patel <iamdhonni@gmail.com>
2026-03-25 21:22:07 +0530

    change

--- 11,2Darray.cpp
+++ 11,2Darray.cpp
@@ -244,17 +244,3 @@ int main () {
 
 
 
-
-bool containsDuplicate(vector<int>& nums) {
-    int n = nums.size();
-
-    for(int i = 0; i < n; i++) {
-        for(int j = i + 1; j < n; j++) {
-            if(nums[i] == nums[j]) {
-                return true;
-            }
-        }
-    }
-
-    return false;
-}
