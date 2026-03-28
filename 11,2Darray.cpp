4dc40bdfe923f4625d46202c5b22ac3e5547284b
Dhoni Patel <iamdhonni@gmail.com>
2026-03-28 21:28:54 +0530

    change

--- 11,2Darray.cpp
+++ 11,2Darray.cpp
@@ -249,15 +249,32 @@ using namespace std;
 
 int main () {
 
-    int n = 4;
-int num = 1;
+//     int n = 4;
+// int num = 1;
+// for(int i = 1; i <= n; i++) {
+//     for(int j = 1; j <= i; j++) {
+//         cout << num << " ";
+//         num++;
+//     }
+//     cout << endl;
+// }
+
+
+int n = 4;
 for(int i = 1; i <= n; i++) {
+    // spaces
+    for(int s = 1; s <= n - i; s++) {
+        cout << " ";
+    }
+    // stars
     for(int j = 1; j <= i; j++) {
-        cout << num << " ";
-        num++;
+        cout << "* ";
     }
     cout << endl;
 }
+
+
+
 
     return 0;
 }
