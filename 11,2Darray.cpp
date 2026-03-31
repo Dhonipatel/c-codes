037e106e72db2656ab8206ecae49572c870c4c00
Dhoni Patel <iamdhonni@gmail.com>
2026-03-31 22:31:53 +0530

    change

--- 11,2Darray.cpp
+++ 11,2Darray.cpp
@@ -285,25 +285,25 @@
 
 // ASSGAIMENT QUESTIONS
 
-#include <iostream>
-using namespace std;
+// #include <iostream>
+// using namespace std;
 
 
-int main () {
-    int arr[2][3] = {{4, 7, 8}, {8, 8, 7}};
-    int n=2;
-    int m=3;
-    int count = 0;
+// int main () {
+//     int arr[2][3] = {{4, 7, 8}, {8, 8, 7}};
+//     int n=2;
+//     int m=3;
+//     int count = 0;
     
-      for(int i=0; i<n; i++) {
-        for(int j=0; j<m; j++) {
-            if(arr[i][j] == 7) {
-                count++;
-            }
-        }
-    }
-    cout<<count<<endl;
+//       for(int i=0; i<n; i++) {
+//         for(int j=0; j<m; j++) {
+//             if(arr[i][j] == 7) {
+//                 count++;
+//             }
+//         }
+//     }
+//     cout<<count<<endl;
   
 
-    return 0;
-}
+//     return 0;
+// }
