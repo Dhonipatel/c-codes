70cf3a8ed1ba59f7fe8a8487effe158b06c185bf
Dhoni Patel <iamdhonni@gmail.com>
2026-04-03 07:18:58 +0530

    change

--- 12CharArray,string.cpp
+++ 12CharArray,string.cpp
@@ -1,25 +1,50 @@
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+
+//     // char ch1 ='a';
+//     // char ch2 = 'X';
+//     // char ch3 = '%';
+//     // char ch4 = '9';
+
+
+//     // char ch1='a';
+//     // cout<<(int) ch1 <<endl;
+
+//     // char ch2='A';
+//     // cout<<(int) ch2 <<endl;
+
+//     char ch1= 'f';
+//     int pos= ch1 - 'a';
+
+//     cout<<pos<<endl;
+
+
+//     return 0;
+// }
+
+
+
 #include <iostream>
 using namespace std;
 
 int main() {
 
-    // char ch1 ='a';
-    // char ch2 = 'X';
-    // char ch3 = '%';
-    // char ch4 = '9';
+    // char arr[5] = {'a', 'b','c','d','e', };
+    // cout << arr<<endl;
 
+    // char work[] = "code";
+    // cout << work << endl;
 
-    // char ch1='a';
-    // cout<<(int) ch1 <<endl;
+    // char work[5] = "code";
+    // cout << work <<endl;
 
-    // char ch2='A';
-    // cout<<(int) ch2 <<endl;
+//     char work[]={'c','o','d','e','\0'};
+//     cout <<work <<endl;
 
-    char ch1= 'f';
-    int pos= ch1 - 'a';
-
-    cout<<pos<<endl;
-
+//     char work [50]= {'c','o','d','e','\0'};
+//     cout << work << endl; 
 
     return 0;
 }
