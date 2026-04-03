30d4f217b01671d59c1f4b30d9901514ccd60dd3
Dhoni Patel <iamdhonni@gmail.com>
2026-04-03 06:48:08 +0530

    change

---
+++ 12CharArray,string.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    // char ch1 ='a';
+    // char ch2 = 'X';
+    // char ch3 = '%';
+    // char ch4 = '9';
+
+
+    char ch1='a';
+    cout<<(int) ch1 <<endl;
+
+    char ch2='A';
+    cout<<(int) ch2 <<endl;
+
+    return 0;
+}
