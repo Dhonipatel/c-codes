074e4dad0f2e7937f8cea97414505b2c46bfcdd2
Dhoni Patel <iamdhonni@gmail.com>
2026-04-06 07:37:52 +0530

    refactor: improve 12CharArray,string

--- 12CharArray,string.cpp
+++ 12CharArray,string.cpp
@@ -107,34 +107,34 @@
 
 // REVERSE  A CHAR ARRAY
 
-#include <iostream>
-#include <cstring>
-using namespace std;
+// #include <iostream>
+// #include <cstring>
+// using namespace std;
 
-void reverse(char word[], int n) {
+// void reverse(char word[], int n) {
 
-  int st = 0, end = n-1;
+//   int st = 0, end = n-1;
 
-  while(st < end) {
-    swap(word[st], word[end]);
-    st++;
-    end--;
-  }
-}
-
-
-int main() {
-
-     char word[] = "code";
-     reverse(word, strlen(word));
-
-     cout << "reverse = " << word << endl;
-     return 0;
+//   while(st < end) {
+//     swap(word[st], word[end]);
+//     st++;
+//     end--;
+//   }
+// }
 
 
+// int main() {
 
-  return 0;
-}
+//      char word[] = "code";
+//      reverse(word, strlen(word));
+
+//      cout << "reverse = " << word << endl;
+//      return 0;
+
+
+
+//   return 0;
+// }
 
 
 
