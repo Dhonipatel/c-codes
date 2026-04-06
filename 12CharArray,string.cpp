2e689deadae9a99a8aeb72d71f6497600e8bba22
Dhoni Patel <iamdhonni@gmail.com>
2026-04-06 08:33:11 +0530

    chnage

--- 12CharArray,string.cpp
+++ 12CharArray,string.cpp
@@ -168,7 +168,25 @@
 
 
 
+// //////// STRING IN ++ //
 
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+
+  string str;
+  getline(cin, str);
+
+  cout << str[0] << endl;
+  cout << str[1] << endl;
+  cout << str[2] << endl;
+
+
+  return 0;
+}
 
 
 
