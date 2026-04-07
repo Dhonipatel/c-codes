8d573c7f4279a3f5c440de380f9329a29a8dae63
Dhoni Patel <iamdhonni@gmail.com>
2026-04-07 07:41:40 +0530

    chore: update 2 file files

--- 12CharArray,string.cpp
+++ 12CharArray,string.cpp
@@ -189,4 +189,27 @@
 // }
 
 
+//STRING IN C++
+// FOR EACH LOOP
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  string str ="apna college !";
+
+  // for (int i=0; i<str.length(); i++) {
+  //   cout << str[i] << "-";
+  // }
+  // cout <<"\n";
+
+  for(char ch : str) {
+    cout << ch <<",";
+    
+  }
+
+  return 0;
+}
+
 
