3f9f63bd2cf929972fcd5ee44e85c114adc8549c
Dhoni Patel <iamdhonni@gmail.com>
2026-04-08 07:23:45 +0530

    change

--- 12CharArray,string.cpp
+++ 12CharArray,string.cpp
@@ -239,3 +239,29 @@
 //     return 0;
 // }
 
+
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+void toUpper(char word[], int n) {
+    for(int i = 0; i < n; i++) {
+        char ch = word[i];
+        if(ch >= 'a' && ch <= 'z') {
+            word[i] = ch - 'a' + 'A';
+        }
+    }
+}
+
+int main() {
+
+    char word[100];
+
+    cin >> word;
+
+    toUpper(word, strlen(word));
+
+    cout << word << endl;
+
+    return 0;
+}
