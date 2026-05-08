03bf6697fd9590ff33160b71b328c5dfc929c3c5
Dhoni Patel <iamdhonni@gmail.com>
2026-05-08 07:44:54 +0530

    change

--- 13Vector.cpp
+++ 13Vector.cpp
@@ -1,44 +1,65 @@
-#include <iostream>
+// #include <iostream>
+// using namespace std;
+
+// void funcArr() {
+//     int size ;
+//     cin >> size;
+
+//     int *ptr = new int [size]; 
+//     int x = 1;
+
+//     for (int i=0; i<size; i++) {
+//         ptr [i] = x;
+
+//         cout << ptr [i] << " ";
+//         x++;
+//     }
+//     cout << endl;
+
+//     delete [] ptr ; // free
+// }
+
+// int main () {
+
+//     funcArr();
+
+
+
+//     // // int arr [100] = {1, 2, 3, 4, 5};
+//     // int size;
+//     // cin >> size;
+
+//     // int *arr = new int(size);
+
+//     // int x = 1;
+//     // for(int i=0; i<size; i++) {
+//     //     arr[i] = x;
+//     //     cout <<arr [i] << " ";
+//     //     x++;
+//     // }
+
+//     cout <<endl;
+
+//     return 0;
+// }
+
+
+#include  <iostream> 
 using namespace std;
 
-void funcArr() {
-    int size ;
-    cin >> size;
+int* func () {
+    int *ptr = new int;
+    *ptr = 1200;
+    cout << " ptr point to " << *ptr << endl;
 
-    int *ptr = new int [size]; 
-    int x = 1;
-
-    for (int i=0; i<size; i++) {
-        ptr [i] = x;
-
-        cout << ptr [i] << " ";
-        x++;
-    }
-    cout << endl;
-
-    delete [] ptr ; // free
+    return ptr;
 }
 
 int main () {
 
-    funcArr();
+    int *x = func();
+    cout << *x<< endl;
     
-
-
-    // // int arr [100] = {1, 2, 3, 4, 5};
-    // int size;
-    // cin >> size;
-
-    // int *arr = new int(size);
-
-    // int x = 1;
-    // for(int i=0; i<size; i++) {
-    //     arr[i] = x;
-    //     cout <<arr [i] << " ";
-    //     x++;
-    // }
-
-    cout <<endl;
 
     return 0;
 }
