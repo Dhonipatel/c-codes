6a472a7270c5d512c95376d24a6d463441a06434
Dhoni Patel <iamdhonni@gmail.com>
2026-05-08 07:26:45 +0530

    change

--- 13Vector.cpp
+++ 13Vector.cpp
@@ -1,19 +1,42 @@
 #include <iostream>
 using namespace std;
 
-int main () {
-    // int arr [100] = {1, 2, 3, 4, 5};
-    int size;
+void funcArr() {
+    int size ;
     cin >> size;
 
-    int *arr = new int(size);
-
+    int *ptr = new int [size]; 
     int x = 1;
-    for(int i=0; i<size; i++) {
-        arr[i] = x;
-        cout <<arr [i] << " ";
+
+    for (int i=0; i<size; i++) {
+        ptr [i] = x;
+
+        cout << ptr [i] << " ";
         x++;
     }
+    cout << endl;
+
+    delete [] ptr ; // free
+}
+
+int main () {
+
+    funcArr();
+    
+
+
+    // // int arr [100] = {1, 2, 3, 4, 5};
+    // int size;
+    // cin >> size;
+
+    // int *arr = new int(size);
+
+    // int x = 1;
+    // for(int i=0; i<size; i++) {
+    //     arr[i] = x;
+    //     cout <<arr [i] << " ";
+    //     x++;
+    // }
 
     cout <<endl;
 
