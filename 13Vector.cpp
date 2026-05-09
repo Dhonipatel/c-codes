2ac305c679fc2c028eeb2f78b88d0e3d2c710f60
Dhoni Patel <iamdhonni@gmail.com>
2026-05-09 08:46:17 +0530

    change

--- 13Vector.cpp
+++ 13Vector.cpp
@@ -98,3 +98,23 @@
 
 //     return 0;
 // }
+
+
+// VECTOR IMPLEMENTATION IN MEMORY
+
+#include <iostream>
+#include<vector>
+using namespace std;
+
+int main () {
+
+    vector<int> vec ={1, 2, 3, 4};
+    cout <<"size :" << vec.size() <<endl;
+    cout << "capacity :"<< vec.capacity() <<endl;
+
+    vec.push_back(5);
+    cout <<"size :" << vec.size() <<endl;
+    cout << "capacity :"<< vec.capacity() <<endl;
+
+    return 0;
+}
