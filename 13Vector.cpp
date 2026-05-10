68513a6e43185075b29150b4d51ea95c2911ef6f
Dhoni Patel <iamdhonni@gmail.com>
2026-05-10 08:54:55 +0530

    change

--- 13Vector.cpp
+++ 13Vector.cpp
@@ -123,38 +123,39 @@
 
 // PAIR SUM
 
-#include <iostream>
-#include <vector>
-using namespace std;
+// #include <iostream>
+// #include <vector>
+// using namespace std;
 
-vector<int> pairSum(vector<int> arr, int target) {
+// vector<int> pairSum(vector<int> arr, int target) {
 
-    int st =0, end =arr.size()-1;
-    int currSum =0;
-    vector<int> ans;
+//     int st =0, end =arr.size()-1;
+//     int currSum =0;
+//     vector<int> ans;
 
-    while(st < end) {
-        currSum = arr[st] + arr[end];
-        if(currSum == target) {
-            ans.push_back(st);
-            ans.push_back(end);
-            return ans;
-        } else if(currSum > target) {
-            end--;
-        }else{
-            st++;
-        }
-    }
-    return ans;
-}
+//     while(st < end) {
+//         currSum = arr[st] + arr[end];
+//         if(currSum == target) {
+//             ans.push_back(st);
+//             ans.push_back(end);
+//             return ans;
+//         } else if(currSum > target) {
+//             end--;
+//         }else{
+//             st++;
+//         }
+//     }
+//     return ans;
+// }
 
-int main(){
+// int main(){
 
-    vector<int>vec = {2, 7, 11, 15};
-    int target =9;
+//     vector<int>vec = {2, 7, 11, 15};
+//     int target =9;
 
-    vector<int> ans = pairSum(vec, target);
-    cout<< ans[0] << "," << ans[1]<<endl;
+//     vector<int> ans = pairSum(vec, target);
+//     cout<< ans[0] << "," << ans[1]<<endl;
 
-    return 0;
-}
+//     return 0;
+// }
+
