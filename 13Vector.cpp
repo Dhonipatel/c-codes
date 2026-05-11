b2e16767ffdd5736861e73411f250d8807be0f0b
Dhoni Patel <iamdhonni@gmail.com>
2026-05-11 22:32:30 +0530

    change

--- 13Vector.cpp
+++ 13Vector.cpp
@@ -183,27 +183,27 @@
 // }
 
 
-#include <iostream>
-using namespace std;
+// #include <iostream>
+// using namespace std;
 
-int main() {
+// int main() {
 
-    int arr[] = {5, 1, 4, 2, 8};
-    int n = 5;
+//     int arr[] = {5, 1, 4, 2, 8};
+//     int n = 5;
 
-    for(int i = 0; i < n-1; i++) {
+//     for(int i = 0; i < n-1; i++) {
 
-        for(int j = 0; j < n-i-1; j++) {
+//         for(int j = 0; j < n-i-1; j++) {
 
-            if(arr[j] > arr[j+1]) {
+//             if(arr[j] > arr[j+1]) {
 
-                swap(arr[j], arr[j+1]);
+//                 swap(arr[j], arr[j+1]);
 
-            }
-        }
-    }
+//             }
+//         }
+//     }
 
-    for(int i = 0; i < n; i++) {
-        cout << arr[i] << " ";
-    }
-}
+//     for(int i = 0; i < n; i++) {
+//         cout << arr[i] << " ";
+//     }
+// }
