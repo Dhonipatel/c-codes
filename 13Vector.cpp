337ee7a821675c1042d4f5dca963dad6cfd1f15b
Dhoni Patel <iamdhonni@gmail.com>
2026-05-13 21:40:56 +0530

    change

--- 13Vector.cpp
+++ 13Vector.cpp
@@ -199,20 +199,20 @@
 //     return 0;
 // }
 
-// int main () {
+int main () {
 
-//     vector <vector<int>> matrix= {{1, 2, 3,},  {4, 5, 6}, {7, 8, 9}};
+    vector <vector<int>> matrix= {{1, 2, 3,},  {4, 5, 6}, {7, 8, 9}};
 
-//     for(int i=0; i<matrix.size();  i++){
-//         for(int j=0; j<matrix[i].size(); j++) {
-//             cout<< matrix[i][j] << " ";
-//         }
-//         cout << endl;
-//     }
+    for(int i=0; i<matrix.size();  i++){
+        for(int j=0; j<matrix[i].size(); j++) {
+            cout<< matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
 
 
 
-//     return 0;
-// }
+    return 0;
+}
 
 
