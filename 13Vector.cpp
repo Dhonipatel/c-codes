c3d2452922cf218e6ba2bd802f932261926ca08f
Dhoni Patel <iamdhonni@gmail.com>
2026-05-19 23:57:35 +0530

    change

--- 13Vector.cpp
+++ 13Vector.cpp
@@ -204,26 +204,26 @@
 
 
 
-class Solution {
-public:
-    bool isPalindrome(int x) {
+// class Solution {
+// public:
+//     bool isPalindrome(int x) {
 
-        if (x < 0) {
-            return false;
-        }
+//         if (x < 0) {
+//             return false;
+//         }
 
-        int original = x;
-        long long reverse = 0;
+//         int original = x;
+//         long long reverse = 0;
 
-        while (x > 0) {
+//         while (x > 0) {
 
-            int digit = x % 10;
+//             int digit = x % 10;
 
-            reverse = reverse * 10 + digit;
+//             reverse = reverse * 10 + digit;
 
-            x = x / 10;
-        }
+//             x = x / 10;
+//         }
 
-        return original == reverse;
-    }
-};
+//         return original == reverse;
+//     }
+// };
