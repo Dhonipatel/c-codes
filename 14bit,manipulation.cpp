a36723bacc80ae49bc32e3066e94edf291d4a66e
Dhoni Patel <iamdhonni@gmail.com>
2026-06-04 07:02:33 +0530

    chnage

--- 14bit,manipulation.cpp
+++ 14bit,manipulation.cpp
@@ -1,21 +1,45 @@
-#include <iostream>
-using namespace std;
-int main() {
+// #include <iostream>
+// using namespace std;
+// int main() {
 
-    // BITWISE OPERATORS
+//     // BITWISE OPERATORS
 
-    cout<<(3 & 5) << endl;
-    cout<<(3 | 5) << endl;
-    cout<<(3 ^ 5) << endl;
+//     cout<<(3 & 5) << endl;
+//     cout<<(3 | 5) << endl;
+//     cout<<(3 ^ 5) << endl;
     
 
-    // BINARY NOT  OPERATOR
+//     // BINARY NOT  OPERATOR
 
-    cout << (~6) << endl; 
-    cout << (~0 )<< endl; 
+//     cout << (~6) << endl; 
+//     cout << (~0 )<< endl; 
 
-    // BINARY SHIFT OPERTORS
-    cout << (7 >> 2) << endl;
+//     // BINARY SHIFT OPERTORS
+//     cout << (7 >> 2) << endl;
+
+//     return 0;
+// }
+
+
+// CHECK NUM ODD AND EVEN
+
+
+#include <iostream>
+using namespace std;
+
+void oddOrEven(int num) {
+    if(!(num & 1)) {
+        cout<<"even"<<endl;
+    } else{
+        cout<<"odd"<<endl;
+    }
+}
+
+int main() {
+
+    oddOrEven(5);
+    oddOrEven(6);
+    oddOrEven(13);
 
     return 0;
 }
