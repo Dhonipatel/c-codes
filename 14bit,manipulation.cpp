984136034fddc8e298c36af71ac84bbe9e3e3f9d
Dhoni Patel <iamdhonni@gmail.com>
2026-06-04 07:26:36 +0530

    change

--- 14bit,manipulation.cpp
+++ 14bit,manipulation.cpp
@@ -24,22 +24,23 @@
 // CHECK NUM ODD AND EVEN
 
 
-#include <iostream>
-using namespace std;
+// #include <iostream>
+// using namespace std;
 
-void oddOrEven(int num) {
-    if(!(num & 1)) {
-        cout<<"even"<<endl;
-    } else{
-        cout<<"odd"<<endl;
-    }
-}
+// void oddOrEven(int num) {
+//     if(!(num & 1)) {
+//         cout<<"even"<<endl;
+//     } else{
+//         cout<<"odd"<<endl;
+//     }
+// }
 
-int main() {
+// int main() {
 
-    oddOrEven(5);
-    oddOrEven(6);
-    oddOrEven(13);
+//     oddOrEven(5);
+//     oddOrEven(6);
+//     oddOrEven(13);
 
-    return 0;
-}
+//     return 0;
+// }
+
