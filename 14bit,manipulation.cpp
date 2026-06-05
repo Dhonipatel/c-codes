c7aeb9bf1f063601953ed0aef57f767d30bec2e9
Dhoni Patel <iamdhonni@gmail.com>
2026-06-05 21:29:20 +0530

    change

--- 14bit,manipulation.cpp
+++ 14bit,manipulation.cpp
@@ -83,14 +83,3 @@ int main() {
     return 0;
 }
 
-// #include <iostream>
-// using namespace std;
-
-// void greet() {
-//     cout << "Hello";
-// }
-
-// int main() {
-//     greet();
-//     return 0;
-// }
