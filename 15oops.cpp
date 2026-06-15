46b3df7735e73b1524f6f3acb8dea7a02432477b
Dhoni Patel <iamdhonni@gmail.com>
2026-06-15 07:49:35 +0530

    change

--- 15oops.cpp
+++ 15oops.cpp
@@ -1,4 +1,35 @@
+// #include <iostream>
+// using namespace std;
+
+// class Student {
+//     public:
+//     string name;
+//     float cgpa;
+
+//     void getpercentage() {
+//         cout<< (cgpa * 10) << "% \n";
+//     }
+// };
+
+// int main () {
+
+//     Student s1;
+
+//     s1.name = "Dhoni";
+//     s1.cgpa= 9.0;
+
+//     cout <<s1.name <<endl;
+//     cout<< s1.cgpa << endl;
+
+//     s1.getpercentage();
+
+//     return 0;
+// }
+
+
+
 #include <iostream>
+#include <String>
 using namespace std;
 
 class Student {
@@ -9,19 +40,38 @@ class Student {
     void getpercentage() {
         cout<< (cgpa * 10) << "% \n";
     }
+
+    // SETTER
+    void setName(string nameVal) {
+        name = nameVal;
+    }
+
+    void setCgpa (float cgpaVal) {
+        cgpa = cgpaVal;
+    }
+
+    // GETTERS
+
+    string getName() {
+        return name;
+    } 
+    float getCgpa(){
+        return cgpa;
+    }
+
 };
 
 int main () {
 
     Student s1;
 
-    s1.name = "Dhoni";
-    s1.cgpa= 9.0;
+    s1.setName ("Dhoni kumar");
+    s1.setCgpa(9.1);
 
-    cout <<s1.name <<endl;
-    cout<< s1.cgpa << endl;
+    cout<< s1.getName()  << endl;
+    cout<<s1.getCgpa() << endl;
 
-    s1.getpercentage();
+    
 
     return 0;
 }
