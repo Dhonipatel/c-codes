ac618a263563020c001222a1e3eace0704a2f9b7
Dhoni Patel <iamdhonni@gmail.com>
2026-06-16 06:51:13 +0530

    change

--- 15oops.cpp
+++ 15oops.cpp
@@ -28,50 +28,101 @@
 
 
 
+// #include <iostream>
+// #include <String>
+// using namespace std;
+
+// class Student {
+//     public:
+//     string name;
+//     float cgpa;
+
+//     void getpercentage() {
+//         cout<< (cgpa * 10) << "% \n";
+//     }
+
+//     // SETTER
+//     void setName(string nameVal) {
+//         name = nameVal;
+//     }
+
+//     void setCgpa (float cgpaVal) {
+//         cgpa = cgpaVal;
+//     }
+
+//     // GETTERS
+
+//     string getName() {
+//         return name;
+//     } 
+//     float getCgpa(){
+//         return cgpa;
+//     }
+
+// };
+
+// int main () {
+
+//     Student s1;
+
+//     s1.setName ("Dhoni kumar");
+//     s1.setCgpa(9.1);
+
+//     cout<< s1.getName()  << endl;
+//     cout<<s1.getCgpa() << endl;
+
+    
+
+//     return 0;
+// }
+
+
+
+// CONSTRUCTOR
+
 #include <iostream>
-#include <String>
 using namespace std;
 
-class Student {
-    public:
-    string name;
-    float cgpa;
+class Car {
+string name;
+string color;
 
-    void getpercentage() {
-        cout<< (cgpa * 10) << "% \n";
-    }
+public :
 
-    // SETTER
-    void setName(string nameVal) {
-        name = nameVal;
-    }
+Car(string nameVal, string colorVal) {
+    cout<<"constructor is called.. object being created ..\n";
+    name = nameVal;
+    color= colorVal;
+}
 
-    void setCgpa (float cgpaVal) {
-        cgpa = cgpaVal;
-    }
+void start() {
+    cout<<"car has start...\n"; 
+}
 
-    // GETTERS
+void stop() {
+    cout<<"car has stopped \n";
+}
 
-    string getName() {
-        return name;
-    } 
-    float getCgpa(){
-        return cgpa;
-    }
+// GETTERS
+
+string getName() {
+    return name;
+}
+
+string getColor() {
+    return color;
+}
 
 };
 
 int main () {
 
-    Student s1;
+    Car c1("maruti 800", "white");
 
-    s1.setName ("Dhoni kumar");
-    s1.setCgpa(9.1);
+    cout<< "car name : " << c1.getName()<<endl;
+    cout<<"car color : " <<c1.getColor() <<endl;
 
-    cout<< s1.getName()  << endl;
-    cout<<s1.getCgpa() << endl;
 
-    
 
     return 0;
 }
