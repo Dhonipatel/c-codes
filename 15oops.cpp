ae050e9ad39b67d90cc636efa4c3f407da4c10a7
Dhoni Patel <iamdhonni@gmail.com>
2026-06-17 07:51:33 +0530

    change

--- 15oops.cpp
+++ 15oops.cpp
@@ -261,52 +261,86 @@
 
 //DESTRUCTOR
 
+// #include <iostream>
+// using namespace std;
+
+// class Car {
+
+//     public:
+//     string name;
+//     string color;
+//     int *mileage;
+
+//     Car(string name, string color) {
+//         this->name = name;
+//         this->color = color;
+//         mileage = new int; //DYNAMIC ALLOCATION
+//         *mileage =12;
+
+//     }
+
+//     Car(Car &original) {
+//         cout<< "copy original to new..\n";
+//         name = original.name;
+//         color = original.color;
+//         //mileage = original.mileage;
+        
+//         // deep copy ke liye
+//         mileage = new int;
+//         *mileage = *original.mileage;
+//     }
+
+//     ~Car() {
+//         cout<<"deleting object..\n";
+//         if(mileage != NULL) {
+//             delete mileage;
+//             mileage = NULL;
+//         }
+//     }
+// };
+
+// int main () {
+
+//     Car c1("maruti 800","white" );
+//     cout<< c1.name<<endl;
+//     cout<<c1.color<<endl;
+//     cout<<*c1.mileage<<endl;
+
+
+
+//     return 0;
+// }
+
+
+
+// INHERITANCE
+
 #include <iostream>
 using namespace std;
 
-class Car {
-
+class Animal{
     public:
-    string name;
     string color;
-    int *mileage;
 
-    Car(string name, string color) {
-        this->name = name;
-        this->color = color;
-        mileage = new int; //DYNAMIC ALLOCATION
-        *mileage =12;
+    void eat (){
+        cout<<"eat..\n";
 
     }
-
-    Car(Car &original) {
-        cout<< "copy original to new..\n";
-        name = original.name;
-        color = original.color;
-        //mileage = original.mileage;
-        
-        // deep copy ke liye
-        mileage = new int;
-        *mileage = *original.mileage;
-    }
-
-    ~Car() {
-        cout<<"deleting object..\n";
-        if(mileage != NULL) {
-            delete mileage;
-            mileage = NULL;
-        }
+    void breathe() {
+        cout<<"breathe ..\n";
     }
 };
 
-int main () {
+class Fish : public Animal{
+    public :
+    int fins;
 
-    Car c1("maruti 800","white" );
-    cout<< c1.name<<endl;
-    cout<<c1.color<<endl;
-    cout<<*c1.mileage<<endl;
+    void swim() {
+        cout<<"swim ..\n";
+    }
+};
 
-
+int main() {
 
     return 0;
 }
