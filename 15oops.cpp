5ff2ecc4fdcd5ee648ff5a4a5634971a2f4146c1
Dhoni Patel <iamdhonni@gmail.com>
2026-06-17 07:23:39 +0530

    change

--- 15oops.cpp
+++ 15oops.cpp
@@ -212,43 +212,49 @@
 
 // SHALLOW AND DEEP COPY
 
-#include <iostream>
-using namespace std;
+// #include <iostream>
+// using namespace std;
 
-class Car {
+// class Car {
 
-    public:
-    string name;
-    string color;
-    int *mileage;
+//     public:
+//     string name;
+//     string color;
+//     int *mileage;
 
-    Car(string name, string color) {
-        this->name = name;
-        this->color = color;
-        mileage = new int; //DYNAMIC ALLOCATION
-        *mileage =12;
+//     Car(string name, string color) {
+//         this->name = name;
+//         this->color = color;
+//         mileage = new int; //DYNAMIC ALLOCATION
+//         *mileage =12;
 
-    }
+//     }
 
-    Car(Car &original) {
-        cout<< "copy original to new..\n";
-        name = original.name;
-        color = original.color;
-        mileage = original.mileage;
-    }
-};
+//     Car(Car &original) {
+//         cout<< "copy original to new..\n";
+//         name = original.name;
+//         color = original.color;
+//         //mileage = original.mileage;
+        
+//         // deep copy ke liye
+//         mileage = new int;
+//         *mileage = *original.mileage;
+//     }
+// };
 
-int main () {
+// int main () {
 
-    Car c1("maruti 800","white" );
+//     Car c1("maruti 800","white" );
 
-    Car c2(c1);
-    cout<< c2.name<<endl;
-    cout<<c2.color<<endl;
-    cout<<*c2.mileage<<endl;
-    *c2.mileage=10;
+//     Car c2(c1);
+//     cout<< c2.name<<endl;
+//     cout<<c2.color<<endl;
+//     cout<<*c2.mileage<<endl;
+//     *c2.mileage=10;
 
-    cout<<*c1.mileage<<endl;
+//     cout<<*c1.mileage<<endl;
+//     cout<<*c2.mileage<<endl;
 
-    return 0;
-}
+//     return 0;
+// }
+
