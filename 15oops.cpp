42c2841dfd16b1ca07c93afaf8528a1af171ed1a
Dhoni Patel <iamdhonni@gmail.com>
2026-06-19 08:28:32 +0530

    change

--- 15oops.cpp
+++ 15oops.cpp
@@ -457,7 +457,7 @@
 // }
 
 
-// //POLYMORPHISM = FUNCTION OVERLOADING 
+// //COMPAILE TIME POLYMORPHISM = FUNCTION OVERLOADING 
 
 // #include <iostream>
 // using namespace std;
@@ -485,47 +485,76 @@
 
 
 
-// //POLYMORPHISM = FUNCTION OVERLOADING 
+// //COMPAILE TIME POLYMORPHISM = FUNCTION OVERLOADING 
+
+// #include <iostream>
+// using namespace std;
+
+// class Complex{
+
+//     int real;
+//     int img;
+
+//     public:
+//     Complex(int r, int i) {
+//         real = r;
+//         img = i;
+
+//     }
+
+//     void showNum() {
+//         cout<< real <<" +" <<img<<"i \n";
+//     }
+
+//     Complex operator + (Complex & c2) {
+//         int resReal = this->real + c2.real;
+//         int resImg = this->img + c2.img;
+//         Complex c3(resReal, resImg);
+//         return c3;
+//     }
+
+// };
+
+// int main () {
+
+//     Complex c1(1, 2);
+//     Complex c2(3, 4);
+
+//     c1.showNum();
+//     c2.showNum();
+
+//     Complex c3= c1 +c2;
+//     c3.showNum();
+//     return 0;
+
+//     return 0;
+// }
+
+
+// RUN TIME POLYMORPHISM = FUNCTION OVERIDING
+
 
 #include <iostream>
 using namespace std;
 
-class Complex{
-
-    int real;
-    int img;
-
-    public:
-    Complex(int r, int i) {
-        real = r;
-        img = i;
-
+class Parent {
+    public :
+    void show(){
+        cout << "parent class show // \n";
     }
+};
 
-    void showNum() {
-        cout<< real <<" +" <<img<<"i \n";
+class Child {
+    public :
+    void show() {
+        cout << "child class show..\n";
     }
-
-    Complex operator + (Complex & c2) {
-        int resReal = this->real + c2.real;
-        int resImg = this->img + c2.img;
-        Complex c3(resReal, resImg);
-        return c3;
-    }
-
 };
 
 int main () {
 
-    Complex c1(1, 2);
-    Complex c2(3, 4);
-
-    c1.showNum();
-    c2.showNum();
-
-    Complex c3= c1 +c2;
-    c3.showNum();
-    return 0;
+    Child child1;
+    child1.show();
 
     return 0;
 }
