b250e45a53ba830c543689e8ef1f4a888ca8ce8c
Dhoni Patel <iamdhonni@gmail.com>
2026-06-20 08:21:13 +0530

    change

--- 15oops.cpp
+++ 15oops.cpp
@@ -599,36 +599,69 @@
 // }
 
 
-//  STATIC KEYWORD
+// //  STATIC KEYWORD
 
-#include <iostream>
-using namespace std;
+// #include <iostream>
+// using namespace std;
 
-// void counter () {
-//     static int count = 0;
-//     count++;
-//     cout<<"count :" << count<<endl;
+// // void counter () {
+// //     static int count = 0;
+// //     count++;
+// //     cout<<"count :" << count<<endl;
+// // }
+
+// class Example{
+//     public:
+//     static int x;
+// };
+// int Example:: x=0;
+
+// int main() {
+//     // counter();
+//     // counter();
+//     // counter();
+//     // counter();
+
+
+//     Example eg1;
+//     Example eg2;
+//     Example eg3;
+
+//     cout<<eg1.x++ <<endl;
+//     cout<<eg2.x++ <<endl;
+//     cout<<eg3.x++ <<endl;
+//     return 0;
 // }
 
-class Example{
-    public:
-    static int x;
-};
-int Example:: x=0;
 
-int main() {
-    // counter();
-    // counter();
-    // counter();
-    // counter();
+// STATIC OBJECT
+
+// #include <iostream>
+// using namespace std;
+
+// class Example {
+
+//     public:
+//      Example () {
+//     cout<<"constructor"<<endl;
+// }
+
+// ~Example() {
+//     cout<<"destructor" <<endl;
+// }
+
+// };
 
 
-    Example eg1;
-    Example eg2;
-    Example eg3;
 
-    cout<<eg1.x++ <<endl;
-    cout<<eg2.x++ <<endl;
-    cout<<eg3.x++ <<endl;
-    return 0;
-}
+// int main() {
+
+//     int a =0;
+//     if(a == 0) {
+//    static Example eg1;
+//     }
+
+//     cout<< "code ending ..\n";
+
+//     return 0;
+// }
