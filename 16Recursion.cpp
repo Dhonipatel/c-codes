473d918e9c0cf10e08d36422572f2a2a4d340a39
Dhoni Patel <iamdhonni@gmail.com>
2026-06-24 08:55:29 +0530

    change

--- 16Recursion.cpp
+++ 16Recursion.cpp
@@ -46,10 +46,23 @@
 
 // SUM OF N NURUAL NUMBERS
 
-#include <iosream>
+#include <iostream>
 using namespace std;
 
+int sum(int n) {
+
+    if(n == 1) {
+        return 1;
+    }
+
+    return n + sum(n-1);
+}
+
+
 int main() {
+
+    cout<< sum(6) <<endl;
+
 
     return 0;
 }
