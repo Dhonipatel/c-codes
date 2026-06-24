ca549d1c30544e0f9ca3db860e57c94d82d4d9e1
Dhoni Patel <iamdhonni@gmail.com>
2026-06-24 08:48:25 +0530

    change

--- 16Recursion.cpp
+++ 16Recursion.cpp
@@ -23,22 +23,33 @@
 
 // PRINT NUMBERS IN DECREASING ORDER
 
-#include <iostream>
+// #include <iostream>
+// using namespace std;
+
+// void print(int n) {
+//     if(n == 0) {
+//         return;
+//     }
+
+//     cout<<n<<endl;
+//     print (n-1);
+// }
+
+// int main() {
+
+//      print(20);
+//     //cout<<ans<<endl;
+
+//     return 0;
+// }
+
+
+// SUM OF N NURUAL NUMBERS
+
+#include <iosream>
 using namespace std;
 
-void print(int n) {
-    if(n == 0) {
-        return;
-    }
-
-    cout<<n<<endl;
-    print (n-1);
-}
-
 int main() {
-
-     print(20);
-    //cout<<ans<<endl;
 
     return 0;
 }
