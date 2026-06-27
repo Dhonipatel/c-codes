4a6b90b8ad369c060d8aff5557a681f0c81152d4
Dhoni Patel <iamdhonni@gmail.com>
2026-06-27 23:07:26 +0530

    changec

--- 16Recursion.cpp
+++ 16Recursion.cpp
@@ -66,3 +66,32 @@ int main() {
 
     return 0;
 }
+
+
+
+
+
+// COUNT SET BIT
+
+#include <iostream>
+using namespace std;
+
+int countSetBits(int num) {
+    int count = 0;
+
+    while(num > 0) {
+        int lastDig = num & 1;
+        count += lastDig;
+
+        num = num >> 1;
+    }
+    cout << count << endl;
+    return count;
+}
+
+int main () {
+
+    countSetBits(10);
+
+    return 0;
+}
