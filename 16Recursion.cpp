d9b2cd9639f9a3c3e6d335e8cb4d7ac2380d61b3
Dhoni Patel <iamdhonni@gmail.com>
2026-07-04 22:25:56 +0530

    change

--- 16Recursion.cpp
+++ 16Recursion.cpp
@@ -89,5 +89,25 @@ int main() {
     return 0;
 }
 
-// 
+// #include <iostream>
+using namespace std;
+
+class Animal {
+public:
+    virtual void sound() {
+        cout << "Animal Sound" << endl;
+    }
+};
+
+class Dog : public Animal {
+public:
+    void sound() override {
+        cout << "Bark" << endl;
+    }
+};
+
+int main() {
+    Animal* a = new Dog();
+    a->sound();
+}
 
