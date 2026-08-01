1062f75619dfd7c036c6b3d1cfd801c61aa092a5
Dhoni Patel <iamdhonni@gmail.com>
2026-08-01 21:53:31 +0530

    change

--- 16Recursion.cpp
+++ 16Recursion.cpp
@@ -91,54 +91,54 @@
 
  
 
-#include <iostream>
-using namespace std;
+// #include <iostream>
+// using namespace std;
 
-bool isSorted(int arr [], int n, int i) {
-    if(i == n-1) {
-        return true;
-    }
+// bool isSorted(int arr [], int n, int i) {
+//     if(i == n-1) {
+//         return true;
+//     }
     
-    if(arr[i] > arr[i+1]) {
-        return false;
-    }
+//     if(arr[i] > arr[i+1]) {
+//         return false;
+//     }
 
-    return isSorted(arr, n, i+1);
-}
+//     return isSorted(arr, n, i+1);
+// }
 
-int main() {
+// int main() {
 
-    int arr1[5] = {1, 2, 3, 4, 5,};// sorted :1
-    int arr2[5] = {1, 2, 4, 3,  5,};//unsorted :0
-    cout<<isSorted( arr2, 5, 0);
+//     int arr1[5] = {1, 2, 3, 4, 5,};// sorted :1
+//     int arr2[5] = {1, 2, 4, 3,  5,};//unsorted :0
+//     cout<<isSorted( arr2, 5, 0);
 
-    return 0;
-};
-
-
-
-// #
+//     return 0;
+// };
 
 
-#include <iostream>
-using namespace std;
 
-class Complex {
-public:
-    int real;
-    int imag;
+// // #
 
-    Complex(int r, int i) {
-        real = r;
-        imag = i;
-    }
 
-    void display() {
-        cout << real << " + " << imag << "i";
-    }
-};
+// #include <iostream>
+// using namespace std;
 
-int main() {
-    Complex c1(3, 4);
-    c1.display();
-}
+// class Complex {
+// public:
+//     int real;
+//     int imag;
+
+//     Complex(int r, int i) {
+//         real = r;
+//         imag = i;
+//     }
+
+//     void display() {
+//         cout << real << " + " << imag << "i";
+//     }
+// };
+
+// int main() {
+//     Complex c1(3, 4);
+//     c1.display();
+// }
