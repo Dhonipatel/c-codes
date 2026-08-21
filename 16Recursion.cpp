f4f3f963fa54cafc62e88c2f6580c1d996994fa5
Dhoni Patel <iamdhonni@gmail.com>
2026-08-21 22:43:23 +0530

    change

--- 16Recursion.cpp
+++ 16Recursion.cpp
@@ -310,40 +310,40 @@
 // }
 
 
-// BINARY SEARCH
+BINARY SEARCH
 
-// #include <iostream>
-// using namespace std;
+#include <iostream>
+using namespace std;
 
-// void binarySearch(int arr[],  int start, int end, int key){
-//     if(start > end) {
-//         return;
-//     }
+void binarySearch(int arr[],  int start, int end, int key){
+    if(start > end) {
+        return;
+    }
 
-//     int mid = (start + end)/2;
+    int mid = (start + end)/2;
 
-//     if(arr[mid] == key){
+    if(arr[mid] == key){
 
-//         cout<<mid<<endl;
-//         return;
+        cout<<mid<<endl;
+        return;
 
-//     }
+    }
 
-//     if(arr[mid] > key) {
-//         return binarySearch(arr , start, mid-1, key);
+    if(arr[mid] > key) {
+        return binarySearch(arr , start, mid-1, key);
 
-//     } 
-//     else {
-//         return binarySearch(arr, mid+1, end, key);
-//     }
-// }
-// int main() {
+    } 
+    else {
+        return binarySearch(arr, mid+1, end, key);
+    }
+}
+int main() {
 
-//     int arr[8] = {1, 2, 3, 4, 5, 6, 7, 8,};
-//     int n= 8;
-//     int key = 5;
+    int arr[8] = {1, 2, 3, 4, 5, 6, 7, 8,};
+    int n= 8;
+    int key = 5;
 
-//     binarySearch(arr, 0, n-1, key);
+    binarySearch(arr, 0, n-1, key);
 
-//     return 0;
-// }
+    return 0;
+}
