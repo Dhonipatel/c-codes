db034780e98d848d53bfa61f956780e03906a7cb
Dhoni Patel <iamdhonni@gmail.com>
2026-08-30 07:55:11 +0530

    change

--- 17Divide,conquer.cpp
+++ 17Divide,conquer.cpp
@@ -60,21 +60,21 @@ int main () {
 }
 
 
-Search
+// Search
 
-int binSearch(int *arr, int n, int key) { int start = 0, end = n-1;
+// int binSearch(int *arr, int n, int key) { int start = 0, end = n-1;
 
-while(start <= end) { int mid = (start + end)
+// while(start <= end) { int mid = (start + end)
 
-}
-if(key == arr[mid]) { return mid;
+// }
+// if(key == arr[mid]) { return mid;
 
-}else if(key > arr[mid]) //search in 2nd half
+// }else if(key > arr[mid]) //search in 2nd half
 
-else {
+// else {
 
-start = mid + 1;
+// start = mid + 1;
 
-//search in 1st half end = mid - 1;
+// //search in 1st half end = mid - 1;
 
-return -1;
+// return -1;
